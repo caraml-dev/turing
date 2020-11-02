53bcc873d8c741a5f387e527ac166f7a1a63f967
David Heryanto <david.heryanto@hotmail.com>
2020-11-02 13:54:18 +0800

    Add register and get function for experiment manager

--- engines/experiment/manager/manager.go
+++ engines/experiment/manager/manager.go
@@ -1,6 +1,11 @@
 package manager
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"sync"
+)
 
 // ExperimentManager describes the methods to be implemented by an experiment engine's Manager,
 // providing access to the information required to set up experiments on Turing.
@@ -29,4 +34,51 @@ type ExperimentManager interface {
 	// ValidateExperimentConfig validates the given Turing experiment config for the expected data and format,
 	// based on the given engine properties
 	ValidateExperimentConfig(Engine, TuringExperimentConfig) error
+}
+
+var managersLock sync.Mutex
+var managers = make(map[string]Factory)
+
+// Factory creates an experiment manager from the provided config.
+//
+// Config is a raw encoded JSON value. The experiment manager implementation
+// for each experiment engine should provide a schema and example
+// of the JSON value to explain the usage.
+type Factory func(config json.RawMessage) (ExperimentManager, error)
+
+// Register an experiment manager with the provided name and factory function.
+//
+// For registration to be properly recorded, Register function should be called in the init
+// phase of the Go execution. The init function is usually defined in the package where
+// the manager is implemented. The name of the experiment manager should be unique
+// across all implementations. Registering multiple experiment managers with the
+// same name will return an error.
+func Register(name string, factory Factory) error {
+	managersLock.Lock()
+	defer managersLock.Unlock()
+
+	name = strings.ToLower(name)
+	if _, found := managers[name]; found {
+		return fmt.Errorf("experiment manager %q was registered twice", name)
+	}
+
+	managers[name] = factory
+	return nil
+}
+
+// Get an experiment manager that has been registered.
+//
+// The manager will be initialized with the provided config using the registered factory function.
+// Retrieving an experiment manager that is not registered will return an error.
+func Get(name string, config json.RawMessage) (ExperimentManager, error) {
+	managersLock.Lock()
+	defer managersLock.Unlock()
+
+	name = strings.ToLower(name)
+	m, ok := managers[name]
+	if !ok {
+		return nil, fmt.Errorf("no experiment manager found for name %s", name)
+	}
+
+	return m(config)
 }
---
+++ engines/experiment/manager/manager_test.go
@@ -0,0 +1,108 @@
+package manager
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+type fakeManager struct {
+	config json.RawMessage
+}
+
+func (fm fakeManager) IsCacheEnabled() bool {
+	return false
+}
+
+func (fm fakeManager) GetEngineInfo() Engine {
+	return Engine{Name: "fake"}
+}
+
+func (fm fakeManager) ListClients() ([]Client, error) {
+	return []Client{}, nil
+}
+
+func (fm fakeManager) ListExperiments() ([]Experiment, error) {
+	return []Experiment{}, nil
+}
+
+func (fm fakeManager) ListExperimentsForClient(client Client) ([]Experiment, error) {
+	return []Experiment{}, nil
+}
+
+func (fm fakeManager) ListVariablesForClient(client Client) ([]Variable, error) {
+	return []Variable{}, nil
+}
+
+func (fm fakeManager) ListVariablesForExperiments(experiments []Experiment) (map[string][]Variable, error) {
+	return map[string][]Variable{}, nil
+}
+
+func (fm fakeManager) GetExperimentRunnerConfig(config TuringExperimentConfig) (json.RawMessage, error) {
+	return nil, nil
+}
+
+func (fm fakeManager) ValidateExperimentConfig(engine Engine, config TuringExperimentConfig) error {
+	return nil
+}
+
+func newFakeManager(config json.RawMessage) (ExperimentManager, error) {
+	return fakeManager{config: config}, nil
+}
+
+func TestRegisterAndGet(t *testing.T) {
+	tests := []struct {
+		name            string
+		managerName     string
+		managerFactory  Factory
+		managerConfig   json.RawMessage
+		skipRegister    bool
+		want            ExperimentManager
+		wantRegisterErr bool
+		wantGetErr      bool
+	}{
+		{
+			name:           "successful registration and retrieval of fakeManager",
+			managerName:    "fakeManager",
+			managerFactory: newFakeManager,
+			managerConfig:  []byte(`{"foo":"bar"}`),
+			want:           fakeManager{config: []byte(`{"foo":"bar"}`)},
+		},
+		{
+			name:            "failed multiple registrations of fakeManager",
+			managerName:     "fakeManager",
+			managerFactory:  newFakeManager,
+			managerConfig:   []byte(`{"foo":"bar"}`),
+			want:            fakeManager{config: []byte(`{"foo":"bar"}`)},
+			wantRegisterErr: true,
+		},
+		{
+			name:         "failed retrieval of non-registered manager",
+			managerName:  "nonRegisteredRunner",
+			skipRegister: true,
+			wantGetErr:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !tt.skipRegister {
+				err := Register(tt.managerName, tt.managerFactory)
+
+				if (err != nil) != tt.wantRegisterErr {
+					t.Errorf("Register() error = %v, wantErr %v", err, tt.wantRegisterErr)
+					return
+				}
+			}
+
+			got, err := Get(tt.managerName, tt.managerConfig)
+			if (err != nil) != tt.wantGetErr {
+				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantGetErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
