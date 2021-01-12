abf698df79e6c072d08815c5c5f485548be904fe
David Heryanto <david.heryanto@hotmail.com>
2021-01-12 13:59:36 +0800

    Update migration script to only add/delete specific json field in log_config

--- api/db-migrations/000007_log_config_kafka_serialization_format.down.sql
+++ api/db-migrations/000007_log_config_kafka_serialization_format.down.sql
@@ -1,14 +1,5 @@
 -- Remove 'serialization_format' field in 'kafka_config'
 
 UPDATE router_versions
-SET log_config = (SELECT json_build_object(
-    'log_level', log_config -> 'log_level',
-        'jaeger_enabled', log_config -> 'jaeger_enabled',
-        'result_logger_type', log_config -> 'result_logger_type',
-        'custom_metrics_enabled', log_config -> 'custom_metrics_enabled',
-        'fiber_debug_log_enabled', log_config -> 'fiber_debug_log_enabled',
-        'kafka_config', json_build_object(
-            'topic', log_config -> 'kafka_config' -> 'topic',
-            'brokers', log_config -> 'kafka_config' -> 'brokers')
-))
+SET log_config = (SELECT log_config #- '{kafka_config,serialization_format}')
 WHERE log_config->>'result_logger_type' = 'kafka';
--- api/db-migrations/000007_log_config_kafka_serialization_format.up.sql
+++ api/db-migrations/000007_log_config_kafka_serialization_format.up.sql
@@ -8,16 +8,8 @@
 -- for all existing rows to 'json'. 
 
 UPDATE router_versions
-SET log_config = (SELECT json_build_object(
-    'log_level', log_config -> 'log_level',
-    'jaeger_enabled', log_config -> 'jaeger_enabled',
-    'result_logger_type', log_config -> 'result_logger_type',
-    'custom_metrics_enabled', log_config -> 'custom_metrics_enabled',
-    'fiber_debug_log_enabled', log_config -> 'fiber_debug_log_enabled',
-    'kafka_config', json_build_object(
-        'serialization_format', 'json',
-        'topic', log_config -> 'kafka_config' -> 'topic',
-        'brokers', log_config -> 'kafka_config' -> 'brokers')
-))
+SET log_config = (
+    SELECT jsonb_set(log_config, '{kafka_config,serialization_format}', '"json"'::jsonb)
+)
 WHERE log_config->>'result_logger_type' = 'kafka' AND
       log_config->'kafka_config'->>'serialization_format' IS NULL;
