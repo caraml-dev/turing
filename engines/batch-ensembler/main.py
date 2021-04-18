2b1f6819efa193e8f2815ba6cc9954d26be9e9c3
Roman Wozniak <roman.wozniak@go-jek.com>
2021-04-18 23:26:05 +0300

    - add description to the argparse argument

--- engines/batch-ensembler/main.py
+++ engines/batch-ensembler/main.py
@@ -5,7 +5,8 @@ import ensembler
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run a PySpark batch ensembling job')
 
-    parser.add_argument('--job-spec', dest='job_spec', type=str)
+    parser.add_argument('--job-spec', dest='job_spec', type=str, required=True,
+                        help='Path to the ensembling job YAML file specification')
 
     args = parser.parse_args()
     print(f'Called with arguments: {args}')
