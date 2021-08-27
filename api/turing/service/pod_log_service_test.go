3bf59d3583f49e27c1dc5983a612b57f7406c53c
Ashwin Chatterji <ashwinath@hotmail.com>
2021-08-27 17:45:29 +0800

    Add test case if there is no logging template given.

--- api/turing/service/pod_log_service_test.go
+++ api/turing/service/pod_log_service_test.go
@@ -42,6 +42,7 @@ func TestPodLogServiceListEnsemblingJobPodLogs(t *testing.T) {
 	}
 	tests := map[string]struct {
 		name       string
+		template   *string
 		args       args
 		controller func() cluster.Controller
 		want       *EnsemblingPodLogs
@@ -53,6 +54,7 @@ func TestPodLogServiceListEnsemblingJobPodLogs(t *testing.T) {
 				componentType:     batch.ImageBuilderPodType,
 				opts:              &PodLogOptions{SinceTime: &sinceTime, HeadLines: &headLines, TailLines: &tailLines},
 			},
+			template: &template,
 			want: &EnsemblingPodLogs{
 				Environment: controllerEnvironmentName,
 				Namespace:   namespace,
@@ -117,6 +119,7 @@ func TestPodLogServiceListEnsemblingJobPodLogs(t *testing.T) {
 				componentType:     batch.DriverPodType,
 				opts:              &PodLogOptions{SinceTime: &sinceTime, HeadLines: &headLines, TailLines: &tailLines},
 			},
+			template: &template,
 			want: &EnsemblingPodLogs{
 				Environment: controllerEnvironmentName,
 				Namespace:   namespace,
@@ -187,6 +190,7 @@ func TestPodLogServiceListEnsemblingJobPodLogs(t *testing.T) {
 				componentType:     batch.ExecutorPodType,
 				opts:              &PodLogOptions{SinceTime: &sinceTime, HeadLines: &headLines, TailLines: &tailLines},
 			},
+			template: &template,
 			want: &EnsemblingPodLogs{
 				Environment: controllerEnvironmentName,
 				Namespace:   namespace,
@@ -250,6 +254,70 @@ func TestPodLogServiceListEnsemblingJobPodLogs(t *testing.T) {
 				return controller
 			},
 		},
+		"success | template not given": {
+			args: args{
+				project:           &client.Project{Name: namespace},
+				ensemblingJobName: ensemblingJobName,
+				componentType:     batch.ImageBuilderPodType,
+				opts:              &PodLogOptions{SinceTime: &sinceTime, HeadLines: &headLines, TailLines: &tailLines},
+			},
+			template: nil,
+			want: &EnsemblingPodLogs{
+				Environment: controllerEnvironmentName,
+				Namespace:   namespace,
+				Logs: []*EnsemblingPodLog{
+					{
+						Timestamp:   time.Date(2020, 7, 7, 7, 0, 5, 0, time.UTC),
+						PodName:     podName,
+						TextPayload: "[INFO] Taking snapshot of full filesystem...",
+					},
+					{
+						Timestamp:   time.Date(2020, 7, 7, 7, 0, 10, 0, time.UTC),
+						PodName:     podName,
+						TextPayload: "[INFO] Pushed image to 1 destinations",
+					},
+				},
+			},
+			controller: func() cluster.Controller {
+				controller := &mocks.Controller{}
+				controller.
+					On("ListPods", namespace, fmt.Sprintf("%s=%s", labeller.GetLabelName(labeller.AppLabel), ensemblingJobName)).
+					Return(&corev1.PodList{
+						Items: []corev1.Pod{
+							{
+								ObjectMeta: metav1.ObjectMeta{Name: "pod-name"},
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{Name: "kaniko"},
+									},
+								},
+							},
+						},
+					},
+						nil,
+					)
+				controller.On(
+					"ListPodLogs",
+					namespace,
+					podName,
+					&corev1.PodLogOptions{
+						Container:  "",
+						Timestamps: true,
+						SinceTime:  &sinceTimeV1Minus1Sec,
+					},
+				).Return(
+					ioutil.NopCloser(
+						bytes.NewBufferString(
+							`2020-07-07T06:59:59Z Please update conda by running
+2020-07-07T07:00:05Z [INFO] Taking snapshot of full filesystem...
+2020-07-07T07:00:10Z [INFO] Pushed image to 1 destinations`,
+						),
+					),
+					nil,
+				)
+				return controller
+			},
+		},
 	}
 
 	for name, tt := range tests {
@@ -260,7 +328,7 @@ func TestPodLogServiceListEnsemblingJobPodLogs(t *testing.T) {
 				clusterControllers,
 				namespace,
 				controllerEnvironmentName,
-				&template,
+				tt.template,
 			)
 			got, _ := s.ListEnsemblingJobPodLogs(
 				tt.args.ensemblingJobName,
