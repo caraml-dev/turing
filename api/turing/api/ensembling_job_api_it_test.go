ec09890fcbce4a646ae2bf9e17740ee54cdce2d8
Ashwin Chatterji <ashwinath@hotmail.com>
2021-09-06 13:17:27 +0800

    Change arguments for List method on ensembling job service.

--- api/turing/api/ensembling_job_api_it_test.go
+++ api/turing/api/ensembling_job_api_it_test.go
@@ -384,7 +384,7 @@ func TestIntegrationEnsemblingJobController_ListEnsemblingJob(t *testing.T) {
 			),
 			ensemblingJobService: func() service.EnsemblingJobService {
 				svc := &mocks.EnsemblingJobService{}
-				svc.On("List", mock.Anything, mock.Anything, mock.Anything).Return(
+				svc.On("List", mock.Anything).Return(
 					&service.PaginatedResults{
 						Results: []interface{}{
 							generateEnsemblingJobFixture(1, models.ID(1), models.ID(1), "test-ensembler-1", true),
@@ -417,7 +417,7 @@ func TestIntegrationEnsemblingJobController_ListEnsemblingJob(t *testing.T) {
 			),
 			ensemblingJobService: func() service.EnsemblingJobService {
 				svc := &mocks.EnsemblingJobService{}
-				svc.On("List", mock.Anything, mock.Anything, mock.Anything).Return(
+				svc.On("List", mock.Anything).Return(
 					&service.PaginatedResults{
 						Results: []interface{}{
 							generateEnsemblingJobFixture(1, models.ID(1), models.ID(1), "test-ensembler-1", true),
@@ -450,7 +450,7 @@ func TestIntegrationEnsemblingJobController_ListEnsemblingJob(t *testing.T) {
 			),
 			ensemblingJobService: func() service.EnsemblingJobService {
 				svc := &mocks.EnsemblingJobService{}
-				svc.On("List", mock.Anything, mock.Anything, mock.Anything).Return(
+				svc.On("List", mock.Anything).Return(
 					&service.PaginatedResults{
 						Results: []interface{}{
 							generateEnsemblingJobFixture(1, models.ID(1), models.ID(1), "test-ensembler-1", true),
@@ -481,7 +481,7 @@ func TestIntegrationEnsemblingJobController_ListEnsemblingJob(t *testing.T) {
 			),
 			ensemblingJobService: func() service.EnsemblingJobService {
 				svc := &mocks.EnsemblingJobService{}
-				svc.On("List", mock.Anything, mock.Anything, mock.Anything).Return(
+				svc.On("List", mock.Anything).Return(
 					&service.PaginatedResults{
 						Results: []interface{}{},
 						Paging: service.Paging{
