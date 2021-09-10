ebc94d35f511b5fe18a3d65f273f1a371fa0cfb8
Roman Wozniak <roman.wozniak@gojek.com>
2021-09-10 12:49:58 +0300

    - improve readability of stripKeys

--- ui/src/utils/object.js
+++ ui/src/utils/object.js
@@ -1,19 +1,19 @@
 export const isEmpty = (obj) => !obj || !Object.keys(obj).length;
 
 // Creates a copy of original object and removes specific keys from it
-export const stripKeys = (obj, keys, deep = true) =>
-  obj !== Object(obj)
-    ? obj
-    : Array.isArray(obj)
-    ? deep
-      ? obj.map((item) => stripKeys(item, keys, deep))
-      : obj
-    : Object.keys(obj)
-        .filter((k) => !keys.includes(k))
-        .reduce(
-          (acc, x) =>
-            Object.assign(acc, {
-              [x]: deep ? stripKeys(obj[x], keys, deep) : obj[x],
-            }),
-          {}
-        );
+export const stripKeys = (obj, keys, deep = true) => {
+  if (obj !== Object(obj)) return obj;
+
+  if (Array.isArray(obj))
+    return deep ? obj.map((item) => stripKeys(item, keys, deep)) : obj;
+
+  return Object.keys(obj)
+    .filter((k) => !keys.includes(k))
+    .reduce(
+      (acc, x) =>
+        Object.assign(acc, {
+          [x]: deep ? stripKeys(obj[x], keys, deep) : obj[x],
+        }),
+      {}
+    );
+};
