e6e12748d36b1d674fee2592f794c90bfd99750d
Ashwin Chatterji <ashwinath@hotmail.com>
2021-10-05 15:40:17 +0800

    Add more context on which line is added.

--- api/turing/models/ensembling_job.go
+++ api/turing/models/ensembling_job.go
@@ -86,6 +86,7 @@ func (r *InfraConfig) Scan(value interface{}) error {
 // but requires some treatment to the overridden name
 func (r *InfraConfig) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
+	// This line is changed because we want the overriden service_account_name to be used
 	toSerialize["service_account_name"] = r.ServiceAccountName
 
 	if r.ArtifactUri != nil {
