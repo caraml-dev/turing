c996a34f3fd3770fc1d52b31505c6aa7eb545c8b
Roman Wozniak <roman.wozniak@gojek.com>
2021-10-06 22:59:50 +0300

    - add entrypoint for turing image to accept UI config

---
+++ entrypoint.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+CMD=()
+TURING_UI_CONFIG_PATH=
+TURING_UI_DIST_DIR=${TURING_UI_DIST_DIR:-}
+TURING_UI_DIST_CONFIG_FILE=${TURING_UI_DIST_DIR}/app.config.js
+TURING_API_BIN=${TURING_API_BIN:?"ERROR: TURING_API_BIN is not specified"}
+
+show_help() {
+  cat <<EOF
+Usage: $(basename "$0") <options> <...>
+    -ui-config               JSON file containing configuration of Turing UI
+EOF
+}
+
+main(){
+  parse_command_line "$@"
+
+  if [[ -n "$TURING_UI_CONFIG_PATH" ]]; then
+    echo "Turing UI config found at ${TURING_UI_CONFIG_PATH}..."
+    if [[ -n "$TURING_UI_DIST_DIR" ]]; then
+      echo "Overriding UI config at $TURING_UI_DIST_CONFIG_FILE"
+
+      echo "var config = $(cat $TURING_UI_CONFIG_PATH);" > "$TURING_UI_DIST_CONFIG_FILE"
+
+      echo "Done."
+    else
+      echo "TURING_UI_DIST_DIR: Turing UI static build directory not provided. Skipping."
+    fi
+  fi
+}
+
+parse_command_line(){
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -ui-config)
+        if [[ -n "$2" ]]; then
+          TURING_UI_CONFIG_PATH="$2"
+          shift
+        else
+          echo "ERROR: '-ui-config' cannot be empty." >&2
+          show_help
+          exit 1
+        fi
+        ;;
+      *)
+        CMD+=("$1")
+        ;;
+    esac
+
+    shift
+  done
+
+  if [[ -n "$TURING_UI_CONFIG_PATH" ]]; then
+    if [ ! -f "$TURING_UI_CONFIG_PATH" ]; then
+      echo "ERROR: config file $TURING_UI_CONFIG_PATH does not exist." >&2
+      show_help
+      exit 1
+    fi
+  fi
+}
+
+main "$@"
+
+exec "$TURING_API_BIN" "${CMD[@]}"
+
