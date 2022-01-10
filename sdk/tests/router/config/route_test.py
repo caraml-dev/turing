50e7343870f318420e7a8568ecde3a98c1d236e8
ewezy <ziyi.ewe@gojek.com>
2022-01-10 17:37:14 +0800

    Add additional tests to increase coverage for Route

--- sdk/tests/router/config/route_test.py
+++ sdk/tests/router/config/route_test.py
@@ -29,3 +29,33 @@ def test_create_route_with_valid_endpoint(id, endpoint, timeout, expected, reque
 def test_create_route_with_invalid_endpoint(id, endpoint, timeout, expected):
     with pytest.raises(expected):
         turing.router.config.route.Route(id, endpoint, timeout)
+
+
+@pytest.mark.parametrize(
+    "id,endpoint,timeout,expected", [
+        pytest.param(
+            "model-a",
+            "http://predict_this.io/model-b",
+            100,
+            "generic_route"
+        )
+    ])
+def test_set_route_with_valid_endpoint(id, endpoint, timeout, expected, request):
+    actual = turing.router.config.route.Route(id, endpoint, timeout)
+    actual.endpoint = "http://predict_this.io/model-a"
+    assert actual.to_open_api() == request.getfixturevalue(expected)
+
+
+@pytest.mark.parametrize(
+    "id,endpoint,timeout,expected", [
+        pytest.param(
+            "model-a",
+            "http://predict_this.io/model-a",
+            100,
+            turing.router.config.route.InvalidUrlException
+        )
+    ])
+def test_set_route_with_invalid_endpoint(id, endpoint, timeout, expected):
+    actual = turing.router.config.route.Route(id, endpoint, timeout)
+    with pytest.raises(expected):
+        actual.endpoint = "http//test_this_route.com/"
