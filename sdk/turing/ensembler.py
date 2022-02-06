3cab3522e97cffb0b29cec8c3534f9b796367542
ewezy <ziyi.ewe@gojek.com>
2022-02-06 23:10:01 +0800

    Modify predict method in SDK PyFunc to allow backward compatibility with current batch ensemblers

--- sdk/turing/ensembler.py
+++ sdk/turing/ensembler.py
@@ -58,7 +58,6 @@ class PyFunc(EnsemblerBase, mlflow.pyfunc.PythonModel, abc.ABC):
 
     def predict(self, context, model_input: pandas.DataFrame) -> \
             Union[numpy.ndarray, pandas.Series, pandas.DataFrame]:
-
         feature_columns = PyFunc._get_columns_with_header(model_input, PyFunc.FEATURE_COLUMN_PREFIX)
         prediction_columns = PyFunc._get_columns_with_header(model_input, PyFunc.PREDICTION_COLUMN_PREFIX)
         treatment_config_columns = PyFunc._get_columns_with_header(model_input, PyFunc.TREATMENT_CONFIG_COLUMN_PREFIX)
@@ -69,7 +68,8 @@ class PyFunc(EnsemblerBase, mlflow.pyfunc.PythonModel, abc.ABC):
             .rename(columns=treatment_config_columns) \
             .apply(lambda row:
                    self.ensemble(
-                       features=row[feature_columns.values()],
+                       features=row[feature_columns.values()] if len(row[feature_columns.values()]) > 0 \
+                           else row.drop(prediction_columns.values()),
                        predictions=row[prediction_columns.values()],
                        treatment_config=row[treatment_config_columns.values()]
                    ), axis=1, result_type='expand')
