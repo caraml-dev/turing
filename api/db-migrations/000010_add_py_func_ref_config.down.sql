f04862dffde3e724ccaf7d177b16ad0fa4fc47e3
ewezy <ziyi.ewe@gojek.com>
2022-02-28 15:55:16 +0800

    Rename py_func_ref_config to pyfunc_config

--- api/db-migrations/000010_add_py_func_ref_config.down.sql
+++ api/db-migrations/000010_add_py_func_ref_config.down.sql
@@ -1,4 +1,4 @@
 -- Removes py_func_ref_config from table.
 -- Hence, this migration involves data loss for ensemblers with type that is "pyfunc"
-ALTER TABLE ensembler_configs DROP COLUMN py_func_ref_config;
+ALTER TABLE ensembler_configs DROP COLUMN pyfunc_config;
 
