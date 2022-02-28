fe8eb1fccc40b1baf5d0b14c76755adc1977b8cd
ewezy <ziyi.ewe@gojek.com>
2022-02-28 16:03:26 +0800

    Rename pyfunc_config to make it consistent with camelcase PyfuncConfig

--- api/db-migrations/000010_add_py_func_ref_config.down.sql
+++
@@ -1,4 +0,0 @@
--- Removes py_func_ref_config from table.
--- Hence, this migration involves data loss for ensemblers with type that is "pyfunc"
-ALTER TABLE ensembler_configs DROP COLUMN pyfunc_config;
-
--- api/db-migrations/000010_add_py_func_ref_config.up.sql
+++
@@ -1,3 +0,0 @@
--- Adds py_func_ref_config to table.
-ALTER TABLE ensembler_configs ADD pyfunc_config jsonb;
-
