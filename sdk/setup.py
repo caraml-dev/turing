c2ac64496b49dbb83ea672d277dd491705570b4d
ewezy <ziyi.ewe@gojek.com>
2022-03-17 13:13:16 +0800

    Update setup tools

--- sdk/setup.py
+++ sdk/setup.py
@@ -30,4 +30,6 @@ setuptools.setup(
         'dev': dev_requirements
     },
     python_requires='>=3.7',
+    long_description=pathlib.Path('./docs/README.md').read_text(),
+    long_descreiption_context_type='text/markdown'
 )
