3fa47bffbb29a0391abb815a0f02a0146c62f197
ewezy <ziyi.ewe@gojek.com>
2022-03-17 17:10:55 +0800

    Refactor confirmation messages

--- ui/src/router/components/form/components/VersionCreationSummary.js
+++ ui/src/router/components/form/components/VersionCreationSummary.js
@@ -5,9 +5,13 @@ export const VersionCreationSummary = ({ router }) => {
   return (
     <Fragment>
       <p>
-        You're about to create a new version for the Turing router{" "}
-        <b>{router.name}</b> in the <b>{router.environment_name}</b> environment
-        without deploying it.
+        You're about to <b>save your changes</b> as a new version for the Turing
+        router <b>{router.name}</b>.
+      </p>
+
+      <p>
+        The new version can be deployed at any time from the router history
+        page.
       </p>
 
       <EuiSpacer size="s" />
