9a354fc536a4450ef95fea43d6968c0075f0dd5d
ewezy <ziyi.ewe@gojek.com>
2022-03-22 15:01:04 +0800

    Add e2e test for creating router versions

---
+++ api/e2e/test/03_create_router_version_test.go
@@ -0,0 +1,148 @@
+// +build e2e
+
+package e2e
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"testing"
+
+	"github.com/tidwall/gjson"
+
+	"github.com/gojek/turing/api/turing/models"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+/*
+Pre:
+testCreateRouter run successfully.
+Steps:
+Create a router version with a valid router config
+b. GET router version 3 > shows status "undeployed"
+c. GET router > config still shows version 1
+d. Test cluster state that all deployments exist
+e. Make a request to the router, validate the response.
+*/
+func TestCreateRouterVersion(t *testing.T) {
+	// Read existing router that MUST have already exists from previous create router e2e test
+	// Router name is assumed to follow this format: e2e-experiment-{{.TestID}}
+	routerName := "e2e-experiment-" + globalTestContext.TestID
+	t.Log(fmt.Sprintf("Retrieving router with name '%s' created from previous test step", routerName))
+	existingRouter, err := getRouterByName(
+		globalTestContext.httpClient, globalTestContext.APIBasePath, globalTestContext.ProjectID, routerName)
+	require.NoError(t, err)
+
+	// Read router config test data
+	data := makeRouterPayload(filepath.Join("testdata", "update_router_high_cpu.json.tmpl"), globalTestContext)
+
+	// Update router
+	url := fmt.Sprintf(
+		"%s/projects/%d/routers/%d/versions",
+		globalTestContext.APIBasePath,
+		globalTestContext.ProjectID,
+		existingRouter.ID,
+	)
+	t.Log("Creating router version: POST " + url)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(data))
+	require.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := globalTestContext.httpClient.Do(req)
+	require.NoError(t, err)
+	assert.Equal(t, http.StatusOK, resp.StatusCode, readBody(t, resp))
+
+	// Get router version 3
+	t.Log("Testing GET router version")
+	routerVersion, err := getRouterVersion(
+		globalTestContext.httpClient,
+		globalTestContext.APIBasePath,
+		globalTestContext.ProjectID,
+		int(existingRouter.ID),
+		3,
+	)
+	require.NoError(t, err)
+	assert.Equal(t, models.RouterVersionStatusUndeployed, routerVersion.Status)
+	assert.NotEmpty(t, routerVersion.Error)
+
+	t.Log("Ensure existing router did not update the version to the new version i.e. the version still unchanged at 1")
+	router, err := getRouter(
+		globalTestContext.httpClient,
+		globalTestContext.APIBasePath,
+		globalTestContext.ProjectID,
+		int(existingRouter.ID),
+	)
+	require.NoError(t, err)
+	require.NotNil(t, router.CurrRouterVersion)
+	assert.Equal(t, 1, int(router.CurrRouterVersion.Version))
+
+	downstream, err := getRouterDownstream(globalTestContext.clusterClients,
+		globalTestContext.ProjectName,
+		fmt.Sprintf("%s-turing-router", router.Name))
+	assert.NoError(t, err)
+	assert.Equal(t, downstream, fmt.Sprintf("%s-turing-router-%d.%s.%s",
+		router.Name, 1, globalTestContext.ProjectName, globalTestContext.KServiceDomain))
+
+	// Check that previous enricher, router, ensembler deployments exist
+	t.Log("Checking cluster state")
+	assert.True(t, isDeploymentExists(
+		globalTestContext.clusterClients,
+		globalTestContext.ProjectName,
+		fmt.Sprintf("%s-turing-router-%d-0-deployment", router.Name, 1)))
+	assert.True(t, isDeploymentExists(
+		globalTestContext.clusterClients,
+		globalTestContext.ProjectName,
+		fmt.Sprintf("%s-turing-enricher-%d-0-deployment", router.Name, 1)))
+	assert.True(t, isDeploymentExists(
+		globalTestContext.clusterClients,
+		globalTestContext.ProjectName,
+		fmt.Sprintf("%s-turing-ensembler-%d-0-deployment", router.Name, 1)))
+
+	// Make request to router
+	t.Log("Testing router endpoint")
+	router, err = getRouter(
+		globalTestContext.httpClient,
+		globalTestContext.APIBasePath,
+		globalTestContext.ProjectID,
+		int(router.ID),
+	)
+	require.NoError(t, err)
+	assert.Equal(t,
+		fmt.Sprintf(
+			"http://%s-turing-router.%s.%s/v1/predict",
+			router.Name,
+			globalTestContext.ProjectName,
+			globalTestContext.KServiceDomain,
+		),
+		router.Endpoint,
+	)
+	req, err = http.NewRequestWithContext(
+		context.Background(),
+		http.MethodPost,
+		router.Endpoint,
+		ioutil.NopCloser(bytes.NewReader([]byte(`{}`))),
+	)
+	require.NoError(t, err)
+	resp, err = globalTestContext.httpClient.Do(req)
+	require.NoError(t, err)
+	responseBytes, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	require.NoError(t, err)
+	actualResponse := gjson.GetBytes(responseBytes, "json.response").String()
+	expectedResponse := `{
+	  "experiment": {},
+	  "route_responses": [
+		{
+		  "data": {
+			"version": "control"
+		  },
+		  "is_default": true,
+		  "route": "control"
+		}
+	  ]
+	}`
+	assert.JSONEq(t, expectedResponse, actualResponse)
+}
