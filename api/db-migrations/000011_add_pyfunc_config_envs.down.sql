e1bea1642ae598c3bf06b5fe43f363aad49609af
ewezy <ziyi.ewe@gojek.com>
2022-03-23 11:09:39 +0800

    Fix typos in migration scripts

--- api/db-migrations/000011_add_pyfunc_config_envs.down.sql
+++ api/db-migrations/000011_add_pyfunc_config_envs.down.sql
@@ -1,10 +1,9 @@
--- Migrate Ensembler rows data from old schema to new schema (with type: docker and docker_config populated
--- from the columns in the old schema)
+-- Migrate pyfunc_config data from new schema to old schema (remove env column)
 UPDATE ensembler_configs
 SET pyfunc_config = (SELECT json_build_object(
-                                    'timeout', pyfunc_config ->> 'timeout',
-                                    'project_id', pyfunc_config ->> 'project_id',
-                                    'ensembler_id', pyfunc_config ->> 'ensembler_id',
-                                    'resource_request', (pyfunc_config ->> 'resource_request')::jsonb,
-                                ))
-WHERE type ='pyfunc'
+        'timeout', pyfunc_config ->> 'timeout',
+        'project_id', pyfunc_config ->> 'project_id',
+        'ensembler_id', pyfunc_config ->> 'ensembler_id',
+        'resource_request', (pyfunc_config ->> 'resource_request')::jsonb
+    ))
+WHERE type ='pyfunc';
--- api/db-migrations/000011_add_pyfunc_config_envs.up.sql
+++ api/db-migrations/000011_add_pyfunc_config_envs.up.sql
@@ -1,19 +1,10 @@
--- Migrate Ensembler rows data from old schema to new schema (with new 'env' field set as an empty array by default)
+-- Migrate pyfunc_config data from old schema to new schema (with new 'env' field set as an empty array by default)
 UPDATE ensembler_configs
-SET docker_config = (SELECT json_build_object(
-                                    'env', '[]'::jsonb,
-                                    'port', docker_config ->> 'port',
-                                    'image', docker_config ->> 'image',
-                                    'timeout', docker_config ->> 'timeout',
-                                    'endpoint', docker_config ->> 'endpoint',
-                                    'service_account', docker_config ->> 'service_account',
-                                    'resource_request', (docker_config ->> 'resource_request')::jsonb,
-                                )),
-    pyfunc_config = (SELECT json_build_object(
-                                    'env', '[]'::jsonb,
-                                    'timeout', pyfunc_config ->> 'timeout',
-                                    'project_id', pyfunc_config ->> 'project_id',
-                                    'ensembler_id', pyfunc_config ->> 'ensembler_id',
-                                    'resource_request', (pyfunc_config ->> 'resource_request')::jsonb,
-                                ))
+SET pyfunc_config = (SELECT json_build_object(
+        'env', '[]'::jsonb,
+        'timeout', pyfunc_config ->> 'timeout',
+        'project_id', pyfunc_config ->> 'project_id',
+        'ensembler_id', pyfunc_config ->> 'ensembler_id',
+        'resource_request', (pyfunc_config ->> 'resource_request')::jsonb
+    ))
 WHERE type ='pyfunc';
