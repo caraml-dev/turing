88299cfa38a5874b5ef96892e38a07fe72fc8b1d
ewezy <ziyi.ewe@gojek.com>
2022-03-23 11:17:25 +0800

    Fix typos in migration scripts

--- api/db-migrations/000011_add_pyfunc_config_envs.up.sql
+++ api/db-migrations/000011_add_pyfunc_config_envs.up.sql
@@ -2,9 +2,9 @@
 UPDATE ensembler_configs
 SET pyfunc_config = (SELECT json_build_object(
         'env', '[]'::jsonb,
-        'timeout', pyfunc_config ->> 'timeout',
-        'project_id', pyfunc_config ->> 'project_id',
-        'ensembler_id', pyfunc_config ->> 'ensembler_id',
+        'timeout', (pyfunc_config ->> 'timeout')::jsonb,
+        'project_id', (pyfunc_config ->> 'project_id')::jsonb,
+        'ensembler_id', (pyfunc_config ->> 'ensembler_id')::jsonb,
         'resource_request', (pyfunc_config ->> 'resource_request')::jsonb
     ))
 WHERE type ='pyfunc';
