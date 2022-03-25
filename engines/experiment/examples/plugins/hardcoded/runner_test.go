8fbaa6e4376d37ad24386870446c989546a75857
Roman Wozniak <roman.wozniak@gojek.com>
2022-03-25 19:07:39 +0800

    - extend unit test for the ExperimentRunner impl

--- engines/experiment/examples/plugins/hardcoded/runner_test.go
+++ engines/experiment/examples/plugins/hardcoded/runner_test.go
@@ -13,6 +13,38 @@ import (
 )
 
 func TestExperimentRunner_GetTreatmentForRequest(t *testing.T) {
+	experiments := []Experiment{
+		{
+			Experiment: manager.Experiment{
+				ID:   "001",
+				Name: "exp_1",
+				Variants: []manager.Variant{
+					{
+						Name: "control",
+					},
+					{
+						Name: "treatment-1",
+					},
+				},
+			},
+			SegmentationConfig: SegmenterConfig{
+				Name:            "customer_id",
+				SegmenterSource: request.PayloadFieldSource,
+				SegmenterValue:  "client.id",
+			},
+			VariantsConfig: map[string]TreatmentConfig{
+				"control": {
+					Traffic: 0.85,
+					Data:    json.RawMessage(`{"foo": "bar"}`),
+				},
+				"treatment-1": {
+					Traffic: 0.15,
+					Data:    json.RawMessage(`{"bar": "baz"}`),
+				},
+			},
+		},
+	}
+
 	suite := map[string]struct {
 		experiments []Experiment
 		header      http.Header
@@ -20,44 +52,28 @@ func TestExperimentRunner_GetTreatmentForRequest(t *testing.T) {
 		expected    *runner.Treatment
 		err         string
 	}{
-		"success": {
-			experiments: []Experiment{
-				{
-					Experiment: manager.Experiment{
-						ID:   "001",
-						Name: "exp_1",
-						Variants: []manager.Variant{
-							{
-								Name: "control",
-							},
-							{
-								Name: "treatment-1",
-							},
-						},
-					},
-					SegmentationConfig: SegmenterConfig{
-						Name:            "customer_id",
-						SegmenterSource: request.PayloadFieldSource,
-						SegmenterValue:  "customer.id",
-					},
-					VariantsConfig: map[string]TreatmentConfig{
-						"control": {
-							Traffic: 0.85,
-							Data:    json.RawMessage(`{"foo": "bar"}`),
-						},
-						"treatment-1": {
-							Traffic: 0.15,
-							Data:    json.RawMessage(`{"bar": "baz"}`),
-						},
-					},
-				},
+		"success | client_id:4": {
+			experiments: experiments,
+			payload:     json.RawMessage(`{"client": {"id": 4}}`),
+			expected: &runner.Treatment{
+				ExperimentName: "exp_1",
+				Name:           "control",
+				Config:         json.RawMessage(`{"foo": "bar"}`),
 			},
-			payload: json.RawMessage(`{"customer": {"id": 7}}`),
+		},
+		"success | client_id:7": {
+			experiments: experiments,
+			payload:     json.RawMessage(`{"client": {"id": 7}}`),
 			expected: &runner.Treatment{
 				ExperimentName: "exp_1",
 				Name:           "treatment-1",
 				Config:         json.RawMessage(`{"bar": "baz"}`),
 			},
+		},
+		"failure": {
+			experiments: experiments,
+			payload:     json.RawMessage(`{}`),
+			err:         "no experiment configured for the unit",
 		},
 	}
 
