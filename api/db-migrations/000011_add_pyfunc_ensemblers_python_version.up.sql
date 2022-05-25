5cbb29681525f2301bce40aa42c45b1797692503
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2022-05-25 14:53:38 +0800

    Update chart values

--- api/db-migrations/000011_add_pyfunc_ensemblers_python_version.up.sql
+++ api/db-migrations/000011_add_pyfunc_ensemblers_python_version.up.sql
@@ -1,3 +1,5 @@
 -- Introduce Python version column, with default as version 3.7 which is the
 -- only supported major version as of the introduction of the column.
-ALTER TABLE pyfunc_ensemblers ADD python_version varchar(16) DEFAULT '3.7.*';
+ALTER TABLE pyfunc_ensemblers ADD python_version varchar(16) NOT NULL DEFAULT '3.7.*';
+-- Drop default after existing rows are updated
+ALTER TABLE pyfunc_ensemblers ALTER COLUMN python_version DROP DEFAULT;
