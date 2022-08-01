35ac1ee70a53ad097ca0a6f25b58cadecc238076
ewezy <ziyi.ewe@gojek.com>
2022-08-01 14:40:19 +0800

    Add hooks to set up turing sdk

--- sdk/e2e/02_update_router_invalid_test.py
+++ sdk/e2e/02_update_router_invalid_test.py
@@ -1,4 +1,7 @@
 import os
+import logging
+
+import requests
 import turing
 
 from turing.router.config.experiment_config import ExperimentConfig
@@ -10,6 +13,7 @@ from turing.router.config.router_version import RouterStatus
 
 def test_update_router_invalid_config():
     # get existing router that has been created in 01_create_router_test.py
+    logging.info("Retrieving router...")
     router = turing.Router.get(1)
     assert router is not None
 
@@ -42,8 +46,51 @@ def test_update_router_invalid_config():
     )
 
     # update router
+    logging.info("Updating router with new config...")
     router.update(new_router_config)
 
     # get router version 2
+    logging.info("Retrieving new router version...")
+    try:
+        router.wait_for_version_status(RouterStatus.FAILED, 2)
+    except TimeoutError:
+        raise Exception(f"Turing API is taking too long for router {router.id} with version 2 to get deployed.")
     router_ver_2 = router.get_version(2)
     assert router_ver_2.status == RouterStatus.FAILED
+
+    # ensure router does not get updated to the failed version (version 2)
+    logging.info("Ensuring the router does not get updated to the failed version (i.e. the version number remains as 1)...")
+    router = turing.Router.get(1)
+    assert router.version == 1
+
+    # test router endpoint by posting a single request
+    assert router.endpoint == f'http://{router.name}-turing-router.{os.getenv("PROJECT_NAME")}.{os.getenv("KSERVICE_DOMAIN")}/v1/predict'
+    logging.info("Testing router endpoint...")
+    response = requests.post(
+        url=router.endpoint,
+        headers={
+            "Content-Type": "application/json",
+            "X-Mirror-Body": "true",
+        },
+        json={
+            "client": {"id": 4}
+        },
+    )
+    assert response.status_code == 200
+    expected_response = {
+        "experiment": {
+            "configuration": {
+                "foo": "bar"
+            }
+        },
+        "route_responses": [
+            {
+                "data": {
+                    "version": "control"
+                },
+                "is_default": False,
+                "route": "control"
+            }
+        ]
+    }
+    assert response.json()['response'] == expected_response
--- sdk/e2e/conftest.py
+++ sdk/e2e/conftest.py
@@ -2,6 +2,6 @@ import os
 import turing
 
 
-def setup_module():
+def pytest_sessionstart():
     turing.set_url(os.getenv('API_BASE_PATH'), use_google_oauth=False)
     turing.set_project(os.getenv('PROJECT_NAME'))
