17f967e14e75e5244ff6b39e45de9db3e1b2abcd
ewezy <ziyi.ewe@gojek.com>
2022-08-05 13:13:49 +0800

    Add sdk e2e test ordering using a pytest hook

--- sdk/e2e/conftest.py
+++ sdk/e2e/conftest.py
@@ -1,5 +1,26 @@
 import os
+
 import turing
+
+TEST_ORDER = [
+    "test_create_router",
+    "test_update_router_invalid_config",
+    "test_create_router_version",
+    "test_undeploy_router",
+    "test_deploy_router_invalid_config",
+    "test_deploy_router_valid_config",
+    "test_delete_router",
+    "test_deploy_router_with_traffic_rules"
+]
+
+
+def pytest_collection_modifyitems(items):
+    # modify test items in place to ensure that the e2e tests run in order
+    item_mappings = {item.originalname: item for item in items}
+    sorted_items = []
+    for test_name in TEST_ORDER:
+        sorted_items.append(item_mappings[test_name])
+    items[:] = sorted_items
 
 
 def pytest_sessionstart():
