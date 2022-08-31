1336128f8e061458a72cda72d4a4cec4f17cede1
Terence Lim <terencelimxp@gmail.com>
2022-08-31 11:58:33 +0800

    Update DB migration SQL queries

--- api/db-migrations/000012_add_traffic_rule_name.down.sql
+++ api/db-migrations/000012_add_traffic_rule_name.down.sql
@@ -1,13 +1,30 @@
 UPDATE router_versions
-SET traffic_rules = s.new_traffic_rules
-FROM (
+SET traffic_rules = combined_rule FROM (
     SELECT
-        jsonb_agg(
-            jsonb_build_object(
-                'conditions', elem -> 'conditions',
-                'routes', elem -> 'routes'
-            )
-        ) new_traffic_rules
+        t1.id
+        , json_agg(t2.unnamed_rule) AS combined_rule
     FROM
-        router_versions, jsonb_array_elements(traffic_rules) as elem
-) s;
+    (
+        (
+            SELECT
+                id,
+                position
+            FROM
+                router_versions,
+                jsonb_array_elements(traffic_rules) with ordinality arr(elem, position)
+        ) AS t1
+        LEFT JOIN
+        (
+        SELECT
+            rv.id,
+            position,
+            elem - 'name' AS unnamed_rule
+        FROM 
+            router_versions rv,
+            jsonb_array_elements(rv.traffic_rules) with ordinality arr(elem, position)
+        ) AS t2
+        ON t1.id = t2.id AND t1.position = t2.position
+    )
+    GROUP BY t1.id
+) AS t3
+WHERE router_versions.id = t3.id;
--- api/db-migrations/000012_add_traffic_rule_name.up.sql
+++ api/db-migrations/000012_add_traffic_rule_name.up.sql
@@ -1,14 +1,42 @@
 UPDATE router_versions
-SET traffic_rules = s.new_traffic_rules
-FROM (
+SET traffic_rules = combined_rule FROM (
     SELECT
-        jsonb_agg(
-            jsonb_build_object(
-                'conditions', elem -> 'conditions',
-                'routes', elem -> 'routes',
-                'name', CONCAT('rule_', position::text)
-            )
-        ) as new_traffic_rules
+        id
+        , json_agg(rule_with_name) AS combined_rule
     FROM
-        router_versions, jsonb_array_elements(traffic_rules) with ordinality arr(elem, position)
-) s;
+        (
+            SELECT
+                t1.id
+                , t1.position
+                , t1.name
+                , t2.elem
+                , jsonb_build_object(
+                    'conditions', t2.elem -> 'conditions',
+                    'routes', t2.elem -> 'routes',
+                    'name', t1.name
+                ) AS rule_with_name
+            FROM
+            (
+                (
+                    SELECT
+                        id,
+                        position,
+                        CONCAT('rule_', position::text) as name
+                    FROM
+                        router_versions,
+                        jsonb_array_elements(traffic_rules) with ordinality arr(elem, position)
+                ) AS t1
+                LEFT JOIN
+                (
+                    SELECT
+                        rv.id,
+                        position,
+                        elem
+                    FROM router_versions rv, jsonb_array_elements(rv.traffic_rules) with ordinality arr(elem, position)
+                ) AS t2
+                ON t1.id = t2.id AND t1.position = t2.position
+            )
+        ) AS t3
+    GROUP BY id
+) AS t4
+WHERE router_versions.id = t4.id;
