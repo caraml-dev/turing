4727a8e0ffcf05ef7a0c8ee77fea5603420d17c5
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2022-09-08 09:50:39 +0800

    Fix migration script

--- api/db-migrations/000013_autoscaling_policy.down.sql
+++ api/db-migrations/000013_autoscaling_policy.down.sql
@@ -1,3 +1,4 @@
 ALTER TABLE enrichers DROP COLUMN autoscaling_policy;
-ALTER TABLE ensembler_configs DROP COLUMN autoscaling_policy;
 ALTER TABLE router_versions DROP COLUMN autoscaling_policy;
+UPDATE ensembler_configs set docker_config = docker_config - 'autoscaling_policy' WHERE type='docker';
+UPDATE ensembler_configs set pyfunc_config = pyfunc_config - 'autoscaling_policy' WHERE type='pyfunc';
