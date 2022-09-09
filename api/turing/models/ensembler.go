377ae0444eb8b7c878f75f03a11d2a9591813ce3
ewezy <ziyi.ewe@gojek.com>
2022-09-09 17:48:24 +0800

    Remove required tag from EnsemblerStandardConfig.RouteNamePath

--- api/turing/models/ensembler.go
+++ api/turing/models/ensembler.go
@@ -33,7 +33,7 @@ const (
 
 type EnsemblerStandardConfig struct {
 	ExperimentMappings []ExperimentMapping `json:"experiment_mappings" validate:"required,dive"`
-	RouteNamePath      string              `json:"route_name_path"  validate:"required"`
+	RouteNamePath      string              `json:"route_name_path"`
 }
 
 type EnsemblerDockerConfig struct {
