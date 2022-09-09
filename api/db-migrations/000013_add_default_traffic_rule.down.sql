5d7068158f4842256b43036acf1d035cfbfbcbb6
Terence Lim <terencelimxp@gmail.com>
2022-09-09 14:12:23 +0800

    Refactor DB scripts

--- api/db-migrations/000013_add_default_traffic_rule.down.sql
+++ api/db-migrations/000013_add_default_traffic_rule.down.sql
@@ -4,6 +4,7 @@ WITH dangling_routes AS (
         , default_traffic_rule -> 'routes' AS routes
     FROM
         router_versions
+    WHERE default_traffic_rule -> 'routes' IS NOT NULL
 ),
 cleaned_router_versions AS (
     SELECT id, CASE WHEN traffic_rules = 'null' THEN '[]'::jsonb ELSE traffic_rules END AS traffic_rules
@@ -15,10 +16,11 @@ exploded_traffic_rules AS (
         , position
         , elem
     FROM cleaned_router_versions, jsonb_array_elements(traffic_rules) WITH ordinality arr(elem, position)
+    WHERE id IN (SELECT id FROM dangling_routes)
 ),
 original_traffic_rules AS (
     SELECT
-        cleaned_router_versions.id
+        exploded_traffic_rules.id
         , position
         , CASE
             WHEN elem IS NOT NULL THEN jsonb_build_object(
@@ -30,19 +32,18 @@ original_traffic_rules AS (
             ELSE NULL
             END AS traffic_rule
     FROM
-        cleaned_router_versions
-        LEFT JOIN exploded_traffic_rules ON cleaned_router_versions.id = exploded_traffic_rules.id
-        LEFT JOIN dangling_routes ON cleaned_router_versions.id = dangling_routes.id
+        exploded_traffic_rules
+        LEFT JOIN dangling_routes ON exploded_traffic_rules.id = dangling_routes.id
 ),
 joined_original_traffic_rules AS (
     SELECT
         id,
-        json_agg(traffic_rule ORDER BY position) FILTER (where traffic_rule IS NOT NULL) traffic_rules_updated
+        json_agg(traffic_rule ORDER BY position) traffic_rules_updated
     FROM original_traffic_rules GROUP BY id
 )
 
 UPDATE router_versions AS t
-SET traffic_rules = (CASE WHEN traffic_rules_updated IS NOT NULL THEN traffic_rules_updated ELSE '[]'::json END)
+SET traffic_rules = traffic_rules_updated
 FROM joined_original_traffic_rules
 WHERE joined_original_traffic_rules.id = t.id;
 
--- api/db-migrations/000013_add_default_traffic_rule.up.sql
+++ api/db-migrations/000013_add_default_traffic_rule.up.sql
@@ -34,11 +34,11 @@ exploded_traffic_rules AS (
         , position
         , elem
     FROM cleaned_router_versions, jsonb_array_elements(traffic_rules) WITH ordinality arr(elem, position)
+    WHERE id IN (SELECT id FROM dangling_routes)
 ),
--- Need to join from cleaned_router_versions since exploded_traffic_rules will exclude router versions which have no traffic rules
 exploded_traffic_rules_with_dangling_routes AS (
     SELECT
-        cleaned_router_versions.id
+        exploded_traffic_rules.id
         , position
         , CASE
             WHEN elem -> 'routes' IS NOT NULL THEN jsonb_build_object(
@@ -49,30 +49,27 @@ exploded_traffic_rules_with_dangling_routes AS (
             ELSE NULL
             END AS traffic_rule
     FROM
-        cleaned_router_versions
-        LEFT JOIN exploded_traffic_rules ON cleaned_router_versions.id = exploded_traffic_rules.id
-        LEFT JOIN dangling_routes ON cleaned_router_versions.id = dangling_routes.id
+        exploded_traffic_rules
+        LEFT JOIN dangling_routes ON exploded_traffic_rules.id = dangling_routes.id
 ),
--- FILTER clause added to prevent aggregating null values into [null] jsonb
 joined_updated_traffic_rules AS (
     SELECT
         id,
-        json_agg(traffic_rule ORDER BY position) FILTER (where traffic_rule IS NOT NULL) traffic_rules_updated
+        json_agg(traffic_rule ORDER BY position) traffic_rules_updated
     FROM exploded_traffic_rules_with_dangling_routes GROUP BY id
 )
 
 UPDATE router_versions AS t 
 SET default_traffic_rule = jsonb_build_object('routes', dangling_routes),
-    traffic_rules = (CASE WHEN traffic_rules_updated IS NOT NULL THEN traffic_rules_updated ELSE '[]'::json END)
+    traffic_rules = traffic_rules_updated
 FROM 
     (
         SELECT
-            router_versions.id,
+            joined_updated_traffic_rules.id,
             dangling_routes,
             traffic_rules_updated
         FROM
-            router_versions
-        LEFT JOIN dangling_routes ON router_versions.id = dangling_routes.id
-        LEFT JOIN joined_updated_traffic_rules ON router_versions.id = joined_updated_traffic_rules.id
+            joined_updated_traffic_rules
+        LEFT JOIN dangling_routes ON joined_updated_traffic_rules.id = dangling_routes.id
     ) AS t2
 WHERE t.id = t2.id;
