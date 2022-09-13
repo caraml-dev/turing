5efd62f417676df90870dc7e8ceffa9be68b0e25
ewezy <ziyi.ewe@gojek.com>
2022-09-13 15:16:26 +0800

    Add additional unit tests for router service creation with std ensemblers with route name path

--- api/turing/cluster/servicebuilder/router_test.go
+++ api/turing/cluster/servicebuilder/router_test.go
@@ -46,7 +46,13 @@ func TestNewRouterService(t *testing.T) {
 	require.NoError(t, err)
 	cfgmapEnsembling, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_ensembling.yml"))
 	require.NoError(t, err)
-	cfgmapStandardEnsemble, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_standard_ensembler.yml"))
+	cfgmapStdEnsemblerWithExpMappings, err := tu.ReadFile(
+		filepath.Join(testDataBasePath, "router_configmap_std_ensembler_with_exp_mappings.yml"),
+	)
+	require.NoError(t, err)
+	cfgmapStdEnsemblerWithRouteNamePath, err := tu.ReadFile(
+		filepath.Join(testDataBasePath, "router_configmap_std_ensembler_with_route_name_path.yml"),
+	)
 	require.NoError(t, err)
 	cfgmapTrafficSplitting, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_traffic_splitting.yml"))
 	require.NoError(t, err)
@@ -257,8 +263,8 @@ func TestNewRouterService(t *testing.T) {
 				UserContainerLimitRequestFactor: 1.5,
 			},
 		},
-		"success | standard ensembler": {
-			filePath:     filepath.Join(testDataBasePath, "router_version_success_standard_ensembler.json"),
+		"success | standard ensembler with experiment mappings": {
+			filePath:     filepath.Join(testDataBasePath, "router_version_success_std_ensembler_with_exp_mappings.json"),
 			expRawConfig: json.RawMessage(expRunnerConfig),
 			expected: &cluster.KnativeService{
 				BaseService: &cluster.BaseService{
@@ -272,7 +278,102 @@ func TestNewRouterService(t *testing.T) {
 					ConfigMap: &cluster.ConfigMap{
 						Name:     "test-svc-turing-fiber-config-1",
 						FileName: "fiber.yml",
-						Data:     string(cfgmapStandardEnsemble),
+						Data:     string(cfgmapStdEnsemblerWithExpMappings),
+						Labels: map[string]string{
+							"app":          "test-svc",
+							"environment":  "",
+							"orchestrator": "turing",
+							"stream":       "test-stream",
+							"team":         "test-team",
+						},
+					},
+					Envs: []corev1.EnvVar{
+						{Name: "APP_NAME", Value: "test-svc-1.test-project"},
+						{Name: "APP_ENVIRONMENT", Value: "test-env"},
+						{Name: "ROUTER_TIMEOUT", Value: "5s"},
+						{Name: "APP_JAEGER_COLLECTOR_ENDPOINT", Value: "jaeger-endpoint"},
+						{Name: "ROUTER_CONFIG_FILE", Value: "/app/config/fiber.yml"},
+						{Name: "APP_SENTRY_ENABLED", Value: "true"},
+						{Name: "APP_SENTRY_DSN", Value: "sentry-dsn"},
+						{Name: "APP_LOGLEVEL", Value: "INFO"},
+						{Name: "APP_CUSTOM_METRICS", Value: "false"},
+						{Name: "APP_JAEGER_ENABLED", Value: "false"},
+						{Name: "APP_RESULT_LOGGER", Value: "bigquery"},
+						{Name: "APP_FIBER_DEBUG_LOG", Value: "false"},
+						{Name: "APP_GCP_PROJECT", Value: "gcp-project-id"},
+						{Name: "APP_BQ_DATASET", Value: "dataset_id"},
+						{Name: "APP_BQ_TABLE", Value: "turing_log_test"},
+						{Name: "APP_BQ_BATCH_LOAD", Value: "false"},
+						{Name: "GOOGLE_APPLICATION_CREDENTIALS", Value: "/var/secret/router-service-account.json"},
+					},
+					Labels: map[string]string{
+						"app":          "test-svc",
+						"environment":  "",
+						"orchestrator": "turing",
+						"stream":       "test-stream",
+						"team":         "test-team",
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: routerConfigMapVolume,
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "test-svc-turing-fiber-config-1",
+									},
+								},
+							},
+						},
+						{
+							Name: secretVolume,
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "service-account",
+									Items: []corev1.KeyToPath{
+										{
+											Key:  secretKeyNameRouter,
+											Path: secretKeyNameRouter,
+										},
+									},
+								},
+							},
+						},
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      routerConfigMapVolume,
+							MountPath: routerConfigMapMountPath,
+						},
+						{
+							Name:      secretVolume,
+							MountPath: secretMountPath,
+						},
+					},
+				},
+				ContainerPort:                   8080,
+				MinReplicas:                     2,
+				MaxReplicas:                     4,
+				TargetConcurrency:               1,
+				QueueProxyResourcePercentage:    20,
+				UserContainerLimitRequestFactor: 1.5,
+			},
+		},
+		"success | standard ensembler with route name path": {
+			filePath:     filepath.Join(testDataBasePath, "router_version_success_std_ensembler_with_route_name_path.json"),
+			expRawConfig: json.RawMessage(expRunnerConfig),
+			expected: &cluster.KnativeService{
+				BaseService: &cluster.BaseService{
+					Name:                 "test-svc-turing-router-1",
+					Namespace:            "test-project",
+					Image:                "asia.gcr.io/gcp-project-id/turing-router:latest",
+					CPURequests:          resource.MustParse("400m"),
+					MemoryRequests:       resource.MustParse("512Mi"),
+					LivenessHTTPGetPath:  "/v1/internal/live",
+					ReadinessHTTPGetPath: "/v1/internal/ready",
+					ConfigMap: &cluster.ConfigMap{
+						Name:     "test-svc-turing-fiber-config-1",
+						FileName: "fiber.yml",
+						Data:     string(cfgmapStdEnsemblerWithRouteNamePath),
 						Labels: map[string]string{
 							"app":          "test-svc",
 							"environment":  "",
