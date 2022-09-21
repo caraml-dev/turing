960402141cd7dfcde284188ba9a0e67a419fb017
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2022-09-21 15:06:44 +0800

    Bugfix: Migration for autoscaling configs when using pyfunc ensemblers

--- api/db-migrations/000014_autoscaling_policy.down.sql
+++ api/db-migrations/000014_autoscaling_policy.down.sql
@@ -1,4 +1,5 @@
 ALTER TABLE enrichers DROP COLUMN autoscaling_policy;
 ALTER TABLE router_versions DROP COLUMN autoscaling_policy;
-UPDATE ensembler_configs set docker_config = docker_config - 'autoscaling_policy' WHERE type='docker';
+UPDATE ensembler_configs set docker_config = docker_config - 'autoscaling_policy'
+    WHERE (type='docker' OR type='pyfunc') AND docker_config IS NOT NULL;
 UPDATE ensembler_configs set pyfunc_config = pyfunc_config - 'autoscaling_policy' WHERE type='pyfunc';
