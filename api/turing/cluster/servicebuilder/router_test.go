a4da4f9ff388bb57f87f89a5664b062d6658c50b
ningjie.lee <ningjie.lee@gojek.com>
2022-10-10 16:24:08 +0800

    add basic upi and http service builder test

--- api/turing/cluster/servicebuilder/router_test.go
+++ api/turing/cluster/servicebuilder/router_test.go
@@ -42,7 +42,9 @@ func TestNewRouterService(t *testing.T) {
 		}
 	}`
 	// Read configmap test data
-	cfgmapDefault, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_default_upi.yml"))
+	cfgmapDefault, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_default.yml"))
+	require.NoError(t, err)
+	cfgmapDefaultUpi, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_default_upi.yml"))
 	require.NoError(t, err)
 	cfgmapEnsembling, err := tu.ReadFile(filepath.Join(testDataBasePath, "router_configmap_ensembling.yml"))
 	require.NoError(t, err)
@@ -64,7 +66,7 @@ func TestNewRouterService(t *testing.T) {
 	// Define tests
 	tests := map[string]testSuiteNewService{
 		"success | basic": {
-			filePath:     filepath.Join(testDataBasePath, "router_version_basic_upi.json"),
+			filePath:     filepath.Join(testDataBasePath, "router_version_basic.json"),
 			expRawConfig: json.RawMessage(expRunnerConfig),
 			expected: &cluster.KnativeService{
 				BaseService: &cluster.BaseService{
@@ -79,6 +81,104 @@ func TestNewRouterService(t *testing.T) {
 						Name:     "test-svc-turing-fiber-config-1",
 						FileName: "fiber.yml",
 						Data:     string(cfgmapDefault),
+						Labels: map[string]string{
+							"app":          "test-svc",
+							"environment":  "",
+							"orchestrator": "turing",
+							"stream":       "test-stream",
+							"team":         "test-team",
+						},
+					},
+					Envs: []corev1.EnvVar{
+						{Name: "APP_NAME", Value: "test-svc-1.test-project"},
+						{Name: "APP_ENVIRONMENT", Value: "test-env"},
+						{Name: "ROUTER_TIMEOUT", Value: "5s"},
+						{Name: "APP_JAEGER_COLLECTOR_ENDPOINT", Value: "jaeger-endpoint"},
+						{Name: "ROUTER_CONFIG_FILE", Value: "/app/config/fiber.yml"},
+						{Name: "ROUTER_PROTOCOL", Value: string(models.HTTP)},
+						{Name: "APP_SENTRY_ENABLED", Value: "true"},
+						{Name: "APP_SENTRY_DSN", Value: "sentry-dsn"},
+						{Name: "APP_LOGLEVEL", Value: "INFO"},
+						{Name: "APP_CUSTOM_METRICS", Value: "false"},
+						{Name: "APP_JAEGER_ENABLED", Value: "false"},
+						{Name: "APP_RESULT_LOGGER", Value: "bigquery"},
+						{Name: "APP_FIBER_DEBUG_LOG", Value: "false"},
+						{Name: "APP_GCP_PROJECT", Value: "gcp-project-id"},
+						{Name: "APP_BQ_DATASET", Value: "dataset_id"},
+						{Name: "APP_BQ_TABLE", Value: "turing_log_test"},
+						{Name: "APP_BQ_BATCH_LOAD", Value: "false"},
+						{Name: "GOOGLE_APPLICATION_CREDENTIALS", Value: "/var/secret/router-service-account.json"},
+					},
+					Labels: map[string]string{
+						"app":          "test-svc",
+						"environment":  "",
+						"orchestrator": "turing",
+						"stream":       "test-stream",
+						"team":         "test-team",
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: routerConfigMapVolume,
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: "test-svc-turing-fiber-config-1",
+									},
+								},
+							},
+						},
+						{
+							Name: secretVolume,
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "service-account",
+									Items: []corev1.KeyToPath{
+										{
+											Key:  secretKeyNameRouter,
+											Path: secretKeyNameRouter,
+										},
+									},
+								},
+							},
+						},
+					},
+					VolumeMounts: []corev1.VolumeMount{
+						{
+							Name:      routerConfigMapVolume,
+							MountPath: routerConfigMapMountPath,
+						},
+						{
+							Name:      secretVolume,
+							MountPath: secretMountPath,
+						},
+					},
+				},
+				ContainerPort:                   8080,
+				Protocol:                        models.HTTP,
+				MinReplicas:                     2,
+				MaxReplicas:                     4,
+				AutoscalingMetric:               "concurrency",
+				AutoscalingTarget:               "1",
+				QueueProxyResourcePercentage:    20,
+				UserContainerLimitRequestFactor: 1.5,
+			},
+		},
+		"success | basic upi": {
+			filePath:     filepath.Join(testDataBasePath, "router_version_basic_upi.json"),
+			expRawConfig: json.RawMessage(expRunnerConfig),
+			expected: &cluster.KnativeService{
+				BaseService: &cluster.BaseService{
+					Name:                 "test-svc-turing-router-1",
+					Namespace:            "test-project",
+					Image:                "asia.gcr.io/gcp-project-id/turing-router:latest",
+					CPURequests:          resource.MustParse("400m"),
+					MemoryRequests:       resource.MustParse("512Mi"),
+					LivenessHTTPGetPath:  "/v1/internal/live",
+					ReadinessHTTPGetPath: "/v1/internal/ready",
+					ConfigMap: &cluster.ConfigMap{
+						Name:     "test-svc-turing-fiber-config-1",
+						FileName: "fiber.yml",
+						Data:     string(cfgmapDefaultUpi),
 						Labels: map[string]string{
 							"app":          "test-svc",
 							"environment":  "",
