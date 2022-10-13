116ffe781e22c5732724a4afbfb574d6aa69545f
aria <pradithya.aria@gmail.com>
2022-10-13 13:58:27 +0800

    UPI Traffic Splitting (#259)
    * Grpc fiber integration with UPI in router (#238)
    
    * update fiber version and patch backward compatibility issues
    
    * update go mod to fork for testing
    
    * update go mod with fiber fix for testing
    
    * update mission control to use grpc fiber on env var
    
    * add missing setup go to workflow and fix linting
    
    * test workflow
    
    * test workflow
    
    * minor refactor on error message
    
    * update fiber version and minor tidy up
    
    * refactor errors from HTTPError to TuringError and logging to support grpc payloads
    
    * fix lint, add logging to grpc call
    
    * refactor metrics logging, extract common func
    
    * refactor folder structure. ignore experiment engine for grpc default turing strat
    
    * fix import
    
    * minor refactoring, add mission_control_grpc test
    
    * shifting logic between grpc and mission control grpc. added mc grpc test
    
    * add test for metrics and grpc server
    
    * add mock and fix metrics test
    
    * add log test
    
    * add benchmarking to upi
    
    * update interceptor as error response are not proto message
    
    * fix typo in benchmark payload
    
    * remove upi payload in http
    
    * refactor bencharmk, remove stopping of server
    
    * add upi large payload for http
    
    * removing upi payload for http missioncontrol as its similar to the existing payload test result
    
    * reverting workflow as not pointing to fork anymore
    
    * fix yaml indent
    
    * fix yaml indent
    
    * fix wrong yaml revert
    
    * renaming of grpc to upi and addition of protocol in config
    
    * Add grpc metadata transmission from fiber endpoint back to client
    
    * update upi proto and benchmark plain grpc gen client
    
    * add mock for transport stream for grpc unit test to work with sending header
    
    * update readme
    
    * refactoring base on PR comments
    
    * refactoring base on PR comments
    
    * create utilty to generate proto for benchmark. remove logger sync
    
    * minor refactoring to comment. change router protocol to be set as default for now
    
    * use latest implementation of fiber who reduce marshalling and usage of dynamicpb
    
    * update go fiber in api server and fix lintint
    
    * using fiber fork. introducing cmux for prom http on grpc
    
    * update fiber version
    
    * update api server config test file
    
    * rework benchmarking for http and grpc
    
    * refactor benchmarking
    
    * add benchmem to benchmark
    
    * refactor test upi server
    
    * minor refactoring on fiber package improts
    
    * fix config test indent
    
    Co-authored-by: ningjie.lee <ningjie.lee@gojek.com>
    
    * Simplify SelectRoute implementation
    
    * Implement traffic splitting for UPI
    
    * Move testutils to allow to be reused outside of fiberapi
    
    * Add test for TestUPIRequest
    
    * Add test for GetValueFromUPIRequest
    
    * Temporarily enable CI
    
    * Fix lint issue in engines/experiment
    
    * Change experiment engine sample base docker image to golang 1.18
    
    * Fix test in routing_strategy_test.go
    
    * Fix lint error in router
    
    * Update go.mod
    
    * Reconfigure CI
    
    * Update openapi spec
    
    * Add traffic rules validation
    
    * Fix lint issue
    
    * Move back testutils to internal package
    
    * Rename GetValueFromRequest to GetValueFromHTTPRequest
    
    * Move traffic rules validation to validation package
    
    * Fix request_test.go
    
    * Rename GetValueFromRequest to GetValueFromHTTPRequest
    
    * Fix lint
    
    Co-authored-by: leonlnj <leenj.leon@gmail.com>
    Co-authored-by: ningjie.lee <ningjie.lee@gojek.com>

--- engines/router/missionctl/fiberapi/traffic_splitting_strategy.go
+++ engines/router/missionctl/fiberapi/traffic_splitting_strategy.go
@@ -3,17 +3,16 @@ package fiberapi
 import (
 	"context"
 	"encoding/json"
-	"net/http"
-	"sync"
 
 	"github.com/caraml-dev/turing/engines/router"
 	"github.com/caraml-dev/turing/engines/router/missionctl/errors"
-	"github.com/caraml-dev/turing/engines/router/missionctl/internal"
 	"github.com/caraml-dev/turing/engines/router/missionctl/log"
+	upiv1 "github.com/caraml-dev/universal-prediction-interface/gen/go/grpc/caraml/upi/v1"
 	"github.com/go-playground/validator/v10"
 	"github.com/go-playground/validator/v10/non-standard/validators"
 	"github.com/gojek/fiber"
 	fiberProtocol "github.com/gojek/fiber/protocol"
+	"google.golang.org/protobuf/proto"
 )
 
 var (
@@ -33,39 +32,48 @@ type TrafficSplittingStrategyRule struct {
 }
 
 // TestRequest checks if the request satisfies all conditions of this rule
-func (r *TrafficSplittingStrategyRule) TestRequest(reqHeader http.Header, bodyBytes []byte) (bool, error) {
-	safeCh := internal.NewSafeChan(1)
-	defer safeCh.Close()
-
-	var wg sync.WaitGroup
-	wg.Add(len(r.Conditions))
-
-	// test each condition asynchronously
-	for _, condition := range r.Conditions {
-		go func(condition *router.TrafficRuleCondition) {
-			res, err := condition.TestRequest(reqHeader, bodyBytes)
+func (r *TrafficSplittingStrategyRule) TestRequest(req fiber.Request) (bool, error) {
+	switch req.Protocol() {
+	case fiberProtocol.HTTP:
+		// test all condition and return immediately if one condition is not satisfied
+		for _, condition := range r.Conditions {
+			res, err := condition.TestRequest(req)
 			if err != nil {
 				log.Glob().Infof(
 					"Failed to test if request matches traffic-splitting condition: %s", err)
 			}
 
 			if !res {
-				safeCh.Write(false)
+				// short circuit
+				return false, nil
+			}
+		}
+	case fiberProtocol.GRPC:
+		// unmarshall outside the loop to minimize execution time
+		// TODO: the unmarshalling can be done more efficiently by using partial deserialization
+		var payload upiv1.PredictValuesRequest
+		if err := proto.Unmarshal(req.Payload(), &payload); err != nil {
+			log.Glob().Errorf(
+				"Failed unmarshalling into UPI request: %s", err)
+			return false, err
+		}
+
+		// test all condition and return immediately if one condition is not satisfied
+		for _, condition := range r.Conditions {
+			res, err := condition.TestUPIRequest(&payload, req.Header())
+			if err != nil {
+				log.Glob().Infof(
+					"Failed to test if request matches traffic-splitting condition: %s", err)
 			}
 
-			wg.Done()
-		}(condition)
+			if !res {
+				// short circuit
+				return false, nil
+			}
+		}
 	}
-
-	// wait for all conditions to be tested and write `true` into results channel
-	go func() {
-		wg.Wait()
-
-		safeCh.Write(true)
-	}()
-
 	// return the first value from the channel
-	return (<-safeCh.Read()).(bool), nil
+	return true, nil
 }
 
 // TrafficSplittingStrategy selects the route based on the traffic splitting
@@ -95,45 +103,19 @@ func (s *TrafficSplittingStrategy) SelectRoute(
 	req fiber.Request,
 	routes map[string]fiber.Component,
 ) (fiber.Component, []fiber.Component, error) {
-	doneCh := make(chan interface{}, 1)
-	errCh := make(chan error, 1)
-
-	defer close(doneCh)
-	defer close(errCh)
-
-	orderedRoutes := []fiber.Component{}
+	var orderedRoutes []fiber.Component
 	// array, that holds results of testing the request by each rule configured on the strategy
 	// `results[k]` – is `true` if the request satisfies `k`th rule of the strategy, and `false`
 	// otherwise
 	results := make([]bool, len(s.Rules))
 
-	var wg sync.WaitGroup
-	wg.Add(len(s.Rules))
-
 	for idx, rule := range s.Rules {
-		// test each rule asynchronously and write results into results array
-		go func(rule *TrafficSplittingStrategyRule, idx int) {
-			// TODO need to handle for grpc
-			if res, err := rule.TestRequest(req.Header(), req.Payload()); err != nil {
-				errCh <- err
-			} else {
-				results[idx] = res
-			}
-			wg.Done()
-		}(rule, idx)
-	}
+		res, err := rule.TestRequest(req)
+		if err != nil {
+			return nil, nil, createFiberError(err, req.Protocol())
+		}
 
-	go func() {
-		wg.Wait()
-		doneCh <- true
-	}()
-
-	// wait for all rules to be tested or until an error appears in error channel
-	select {
-	case <-doneCh:
-	case err := <-errCh:
-		log.WithContext(ctx).Errorf(err.Error())
-		return nil, nil, createFiberError(err, fiberProtocol.HTTP)
+		results[idx] = res
 	}
 
 	// select primary route and fallbacks, based on the results of testing
@@ -146,7 +128,7 @@ func (s *TrafficSplittingStrategy) SelectRoute(
 			} else {
 				err := errors.Newf(errors.BadConfig, `route with id "%s" doesn't exist in the router`, routeID)
 				log.WithContext(ctx).Errorf(err.Error())
-				return nil, nil, createFiberError(err, fiberProtocol.HTTP)
+				return nil, nil, createFiberError(err, req.Protocol())
 			}
 		}
 	}
@@ -158,7 +140,7 @@ func (s *TrafficSplittingStrategy) SelectRoute(
 		} else {
 			err := errors.Newf(errors.NotFound, "http request didn't match any traffic rule")
 			log.WithContext(ctx).Errorf(err.Error())
-			return nil, nil, createFiberError(err, fiberProtocol.HTTP)
+			return nil, nil, createFiberError(err, req.Protocol())
 		}
 	}
 
