5a89e8190e3758d957b1201676643710c1e9ecbb
Pradithya Aria <pradithya.pura@go-jek.com>
2022-10-17 18:56:13 +0800

    Print type

--- engines/router/missionctl/fiberapi/routing_strategy.go
+++ engines/router/missionctl/fiberapi/routing_strategy.go
@@ -69,7 +69,7 @@ func (r *DefaultTuringRoutingStrategy) SelectRoute(
 	case fiberProtocol.GRPC:
 		upiRequest, ok := req.(*upi.Request)
 		if !ok {
-			err := fmt.Errorf("failed to convert into UPI request %v", req)
+			err := fmt.Errorf("failed to convert into UPI request %t", req)
 			log.Glob().Error(err.Error())
 			return nil, nil, err
 		}
--- engines/router/missionctl/fiberapi/traffic_splitting_strategy.go
+++ engines/router/missionctl/fiberapi/traffic_splitting_strategy.go
@@ -51,7 +51,7 @@ func (r *TrafficSplittingStrategyRule) TestRequest(req fiber.Request) (bool, err
 	case fiberProtocol.GRPC:
 		upiRequest, ok := req.(*upi.Request)
 		if !ok {
-			err := fmt.Errorf("failed to convert into UPI request %v", req)
+			err := fmt.Errorf("failed to convert into UPI request %t", req)
 			log.Glob().Error(err.Error())
 			return false, err
 		}
