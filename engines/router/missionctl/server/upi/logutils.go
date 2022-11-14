f9cf2c8ccb283ecf47ce1228619912b734cfb0e4
Krithika Sundararajan <krithika.sundararajan@go-jek.com>
2022-11-14 13:50:18 +0800

    Revert "Update router Errorw logs to Warnw"
    This reverts commit 09ca9612c61712d91f729377c9eea10d07e6e9e4.

--- engines/router/missionctl/server/upi/logutils.go
+++ engines/router/missionctl/server/upi/logutils.go
@@ -59,7 +59,7 @@ func logTuringRouterRequestError(ctx context.Context, err *errors.TuringError) {
 	defer func() {
 		_ = logger.Sync()
 	}()
-	logger.Warnw("Turing Request Error",
+	logger.Errorw("Turing Request Error",
 		"error", err.Message,
 		"status", err.Code,
 	)
