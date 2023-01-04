c10b89ed69c800a5070386c76176eee54bd6a21a
ewezy <ziyi.ewe@gojek.com>
2023-01-04 14:15:59 +0800

    Add additional inline comments to experiment runner

--- engines/experiment/runner/runner.go
+++ engines/experiment/runner/runner.go
@@ -21,17 +21,23 @@ type Treatment struct {
 // MetricsRegistrationHelper is the generic interface for the Turing router to
 // register additional metrics needed by the experiment engine
 type MetricsRegistrationHelper interface {
+	// Register is a method that should be called within ExperimentRunner.RegisterMetricsCollector to register
+	// additional metrics that the experiment engine requires on the metrics collector of the Turing Router
 	Register(metrics []instrumentation.Metric) error
 }
 
 // ExperimentRunner is the generic interface for generating experiment configs
 // for a given request
 type ExperimentRunner interface {
+	// GetTreatmentForRequest is a method that is called by the Turing Router for each request it receives to retrieve
+	// an appropriate treatment for it
 	GetTreatmentForRequest(
 		header http.Header,
 		payload []byte,
 		options GetTreatmentOptions,
 	) (*Treatment, error)
+	// RegisterMetricsCollector is a method that should only be called on startup of the experiment runner to register
+	// the metrics collector of the Turing Router (to be called just after the experiment runner has been initialised)
 	RegisterMetricsCollector(
 		collector metrics.Collector,
 		metricsRegistrationHelper MetricsRegistrationHelper,
