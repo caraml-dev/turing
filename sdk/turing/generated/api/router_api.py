c90cd758eb36197c14bf2a9e27adffe30c506b18
Alexander <alexander.i.interns@aux.gojek.com>
2023-04-19 16:21:53 +0700

    feat: add router version for ensembler API

--- sdk/turing/generated/api/router_api.py
+++ sdk/turing/generated/api/router_api.py
@@ -29,6 +29,7 @@ from turing.generated.model.router_id_and_version import RouterIdAndVersion
 from turing.generated.model.router_id_object import RouterIdObject
 from turing.generated.model.router_version import RouterVersion
 from turing.generated.model.router_version_config import RouterVersionConfig
+from turing.generated.model.router_version_status import RouterVersionStatus
 
 
 class RouterApi(object):
@@ -1722,4 +1723,137 @@ class RouterApi(object):
             },
             api_client=api_client,
             callable=__projects_project_id_routers_router_id_versions_version_get
+        )
+
+        def __projects_project_id_routers_version_ensembler_ensembler_id_get(
+            self,
+            project_id,
+            ensembler_id,
+            **kwargs
+        ):
+            """Returns Router Version based on ensembler  # noqa: E501
+
+            This method makes a synchronous HTTP request by default. To make an
+            asynchronous HTTP request, please pass async_req=True
+
+            >>> thread = api.projects_project_id_routers_version_ensembler_ensembler_id_get(project_id, ensembler_id, async_req=True)
+            >>> result = thread.get()
+
+            Args:
+                project_id (int):
+                ensembler_id (int):
+
+            Keyword Args:
+                status ([RouterVersionStatus]): [optional]
+                _return_http_data_only (bool): response data without head status
+                    code and headers. Default is True.
+                _preload_content (bool): if False, the urllib3.HTTPResponse object
+                    will be returned without reading/decoding response data.
+                    Default is True.
+                _request_timeout (float/tuple): timeout setting for this request. If one
+                    number provided, it will be total request timeout. It can also
+                    be a pair (tuple) of (connection, read) timeouts.
+                    Default is None.
+                _check_input_type (bool): specifies if type checking
+                    should be done one the data sent to the server.
+                    Default is True.
+                _check_return_type (bool): specifies if type checking
+                    should be done one the data received from the server.
+                    Default is True.
+                _host_index (int/None): specifies the index of the server
+                    that we want to use.
+                    Default is read from the configuration.
+                async_req (bool): execute request asynchronously
+
+            Returns:
+                [RouterVersion]
+                    If the method is called asynchronously, returns the request
+                    thread.
+            """
+            kwargs['async_req'] = kwargs.get(
+                'async_req', False
+            )
+            kwargs['_return_http_data_only'] = kwargs.get(
+                '_return_http_data_only', True
+            )
+            kwargs['_preload_content'] = kwargs.get(
+                '_preload_content', True
+            )
+            kwargs['_request_timeout'] = kwargs.get(
+                '_request_timeout', None
+            )
+            kwargs['_check_input_type'] = kwargs.get(
+                '_check_input_type', True
+            )
+            kwargs['_check_return_type'] = kwargs.get(
+                '_check_return_type', True
+            )
+            kwargs['_host_index'] = kwargs.get('_host_index')
+            kwargs['project_id'] = \
+                project_id
+            kwargs['ensembler_id'] = \
+                ensembler_id
+            return self.call_with_http_info(**kwargs)
+
+        self.projects_project_id_routers_version_ensembler_ensembler_id_get = _Endpoint(
+            settings={
+                'response_type': ([RouterVersion],),
+                'auth': [],
+                'endpoint_path': '/projects/{project_id}/routers-version-ensembler/{ensembler_id}',
+                'operation_id': 'projects_project_id_routers_version_ensembler_ensembler_id_get',
+                'http_method': 'GET',
+                'servers': None,
+            },
+            params_map={
+                'all': [
+                    'project_id',
+                    'ensembler_id',
+                    'status',
+                ],
+                'required': [
+                    'project_id',
+                    'ensembler_id',
+                ],
+                'nullable': [
+                ],
+                'enum': [
+                ],
+                'validation': [
+                ]
+            },
+            root_map={
+                'validations': {
+                },
+                'allowed_values': {
+                },
+                'openapi_types': {
+                    'project_id':
+                        (int,),
+                    'ensembler_id':
+                        (int,),
+                    'status':
+                        ([RouterVersionStatus],),
+                },
+                'attribute_map': {
+                    'project_id': 'project_id',
+                    'ensembler_id': 'ensembler_id',
+                    'status': 'status',
+                },
+                'location_map': {
+                    'project_id': 'path',
+                    'ensembler_id': 'path',
+                    'status': 'query',
+                },
+                'collection_format_map': {
+                    'status': 'multi',
+                }
+            },
+            headers_map={
+                'accept': [
+                    'application/json'
+                ],
+                'content_type': [],
+            },
+            api_client=api_client,
+            callable=__projects_project_id_routers_version_ensembler_ensembler_id_get
         )
