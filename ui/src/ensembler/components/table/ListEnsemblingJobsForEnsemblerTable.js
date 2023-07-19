b43855612b19d986758f37cfd108a97358e8259d
ewezy <ziyi.ewe@gojek.com>
2023-07-19 15:03:23 +0800

    Refactor deletion messages for ensembling jobs to reflect singularity/plurality

--- ui/src/ensembler/components/table/ListEnsemblingJobsForEnsemblerTable.js
+++ ui/src/ensembler/components/table/ListEnsemblingJobsForEnsemblerTable.js
@@ -93,7 +93,8 @@ export const ListEnsemblingJobsForEnsemblerTable = ({
       {canDeleteEnsembler ? ( results.totalInactiveCount > 0 && (
         <div>
           <br/>
-          <p>Deleting this Ensembler will also delete {results.totalInactiveCount} <b>Failed</b> or <b>Completed</b> Ensembling Jobs that use this Ensembler </p>
+          Deleting this Ensembler will also delete {results.totalInactiveCount} <b>Failed</b> or <b>Completed</b>
+          &nbsp;Ensembling Job{results.totalInactiveCount > 1 && "s"} that use{results.totalInactiveCount === 1 && "s"} this Ensembler:
           <EuiBasicTable
             items={results.inactiveItems}
             loading={!isLoaded}
@@ -105,8 +106,8 @@ export const ListEnsemblingJobsForEnsemblerTable = ({
       )) : ( results.totalActiveCount > 0 && (
         <div>
           <br/>
-          <p>This Ensembler is being used by {results.totalActiveCount} <b>Active Ensembling Jobs</b>. 
-          If the ensembling jobs are in terminating state, please wait until the process complete to delete this ensembler</p>
+          This Ensembler is used by {results.totalActiveCount} <b>Active Ensembling Job{results.totalActiveCount > 1 && "s"}</b>.
+          If any ensembling jobs are in the terminating state, please wait until they are complete to delete this ensembler.
           <EuiBasicTable
             items={results.activeItems}
             loading={!isLoaded}
@@ -114,6 +115,7 @@ export const ListEnsemblingJobsForEnsemblerTable = ({
             responsive={true}
             tableLayout="auto"
           />
+          If you wish to delete this ensembler, please <b>terminate</b> {results.totalActiveCount > 1 ? "these ensembling jobs" : "this ensembling job"}.
         </div>
       ))}
     </Fragment>
