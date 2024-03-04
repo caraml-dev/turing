ad632081158d784afac47cb6b98109ebc357cd20
ewezy <ziyi.ewe@gojek.com>
2024-03-04 14:14:57 +0800

    Remove ticks and display min max labels in EuiDualRange slider

--- ui/src/router/components/form/components/ResourcesPanel.js
+++ ui/src/router/components/form/components/ResourcesPanel.js
@@ -12,8 +12,6 @@ import {
 import { FormLabelWithToolTip } from "../../../../components/form/label_with_tooltip/FormLabelWithToolTip";
 import { useOnChangeHandler } from "../../../../components/form/hooks/useOnChangeHandler";
 
-const maxTicks = 20;
-
 export const ResourcesPanel = ({
   resourcesConfig,
   onChangeHandler,
@@ -90,10 +88,8 @@ export const ResourcesPanel = ({
             fullWidth
             min={0}
             max={maxAllowedReplica}
-            // This component only allows up to 20 ticks to be displayed at the slider
-            tickInterval={Math.ceil((maxAllowedReplica + 1) / maxTicks)}
             showInput
-            showTicks
+            showLabels
             value={[
               resourcesConfig.min_replica || 0,
               resourcesConfig.max_replica || 0,
