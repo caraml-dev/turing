c723264d40864809923b7566bcff6ed5be9b5694
ewezy <ziyi.ewe@gojek.com>
2024-03-14 18:16:02 +0800

    Fix bug in unit tests where mutex locks were copied

--- api/turing/internal/testutils/validation.go
+++ api/turing/internal/testutils/validation.go
@@ -9,13 +9,13 @@ import (
 )
 
 // CompareObjects checks equality of 2 objects and returns a formatted error on failure.
-// All nested unexported fields are ignored. See DeepAllowUnexported for more details.
+// If the object types have unexported fields, a custom marshaler is required to be defined.
 func CompareObjects(actual interface{}, expected interface{}) error {
 	allowUnexportedOn := actual
 	if reflect.TypeOf(allowUnexportedOn).Kind() == reflect.Ptr {
 		allowUnexportedOn = reflect.ValueOf(actual).Elem().Interface()
 	}
-	if !cmp.Equal(actual, expected, DeepAllowUnexported(allowUnexportedOn, expected)) {
+	if !cmp.Equal(actual, expected, cmp.AllowUnexported(allowUnexportedOn)) {
 		actualString := fmt.Sprintf("%+v", actual)
 		expectedString := fmt.Sprintf("%+v", expected)
 
@@ -33,48 +33,4 @@ func CompareObjects(actual interface{}, expected interface{}) error {
 			expectedString, actualString)
 	}
 	return nil
-}
-
-// DeepAllowUnexported allows nested unexported fields to be allowed. This compare function is found here:
-// https://github.com/google/go-cmp/issues/40#issuecomment-328615283
-func DeepAllowUnexported(vs ...interface{}) cmp.Option {
-	m := make(map[reflect.Type]struct{})
-	for _, v := range vs {
-		structTypes(reflect.ValueOf(v), m)
-	}
-	var typs []interface{}
-	for t := range m {
-		typs = append(typs, reflect.New(t).Elem().Interface())
-	}
-	return cmp.AllowUnexported(typs...)
-}
-
-func structTypes(v reflect.Value, m map[reflect.Type]struct{}) {
-	if !v.IsValid() {
-		return
-	}
-	switch v.Kind() {
-	case reflect.Ptr:
-		if !v.IsNil() {
-			structTypes(v.Elem(), m)
-		}
-	case reflect.Interface:
-		if !v.IsNil() {
-			structTypes(v.Elem(), m)
-		}
-	case reflect.Slice, reflect.Array:
-		for i := 0; i < v.Len(); i++ {
-			structTypes(v.Index(i), m)
-		}
-	case reflect.Map:
-		for _, k := range v.MapKeys() {
-			structTypes(v.MapIndex(k), m)
-		}
-	case reflect.Struct:
-		m[v.Type()] = struct{}{}
-		for i := 0; i < v.NumField(); i++ {
-			structTypes(v.Field(i), m)
-		}
-	default:
-	}
 }
