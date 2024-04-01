f9a90caee99182d33f6d090a0e9d66671f08045f
Shide Foo <shidefoo@gmail.com>
2024-04-01 15:46:33 +0800

    Update labeller

--- api/turing/cluster/labeller/labeller.go
+++ api/turing/cluster/labeller/labeller.go
@@ -1,6 +1,11 @@
 package labeller
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+
+	mlp "github.com/caraml-dev/mlp/api/client"
+)
 
 const (
 	// orchestratorValue is the value of the orchestrator (which is Turing)
@@ -22,6 +27,31 @@ var (
 	environment string
 )
 
+var validLabelRegex = regexp.MustCompile("^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$")
+
+var reservedKeys = map[string]bool{
+	orchestratorLabel: true,
+	environmentLabel:  true,
+	streamLabel:       true,
+	teamLabel:         true,
+	AppLabel:          true,
+}
+
+// ValidLabel logic reused from
+// https://github.com/caraml-dev/merlin/blob/06f121c6da05c5b5f1a28389e1078aaafed67541/api/models/metadata.go#L57
+func IsValidLabel(name string) error {
+	lengthOfName := len(name) < 64
+	if !(lengthOfName) {
+		return fmt.Errorf("length of name is greater than 63 characters")
+	}
+
+	if isValidName := validLabelRegex.MatchString(name); !isValidName {
+		return fmt.Errorf("name violates kubernetes label constraint")
+	}
+
+	return nil
+}
+
 // InitKubernetesLabeller builds a new KubernetesLabeller Singleton
 func InitKubernetesLabeller(p, e string) {
 	prefix = p
@@ -33,6 +63,7 @@ type KubernetesLabelsRequest struct {
 	Stream string
 	Team   string
 	App    string
+	Labels []mlp.Label
 }
 
 // GetLabelName prefixes the label with the config specified label and returns the formatted label name
@@ -41,12 +72,32 @@ func GetLabelName(name string) string {
 }
 
 // BuildLabels builds the labels for the Kubernetes object
+// Combines resource labels with project labels
 func BuildLabels(r KubernetesLabelsRequest) map[string]string {
-	return map[string]string{
+	labels := map[string]string{
 		GetLabelName(orchestratorLabel): orchestratorValue,
 		GetLabelName(streamLabel):       r.Stream,
 		GetLabelName(teamLabel):         r.Team,
 		GetLabelName(AppLabel):          r.App,
 		GetLabelName(environmentLabel):  environment,
 	}
+	for _, label := range r.Labels {
+		// skip label that is trying to override reserved key
+		if _, usingReservedKeys := reservedKeys[prefix+label.Key]; usingReservedKeys {
+			continue
+		}
+
+		// skip label that has invalid key name
+		if err := IsValidLabel(label.Key); err != nil {
+			continue
+		}
+
+		// skip label that has invalid value name
+		if err := IsValidLabel(label.Value); err != nil {
+			continue
+		}
+
+		labels[label.Key] = label.Value
+	}
+	return labels
 }
--- api/turing/cluster/labeller/labeller_test.go
+++ api/turing/cluster/labeller/labeller_test.go
@@ -1,6 +1,11 @@
 package labeller
 
-import "testing"
+import (
+	"testing"
+
+	mlp "github.com/caraml-dev/mlp/api/client"
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestLabeller(t *testing.T) {
 	tests := map[string]struct {
@@ -44,6 +49,83 @@ func TestLabeller(t *testing.T) {
 				if _, ok := labels[key]; !ok {
 					t.Errorf("expected key %s", key)
 				}
+			}
+		})
+	}
+}
+
+func TestBuildLabels(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  KubernetesLabelsRequest
+		expected map[string]string
+	}{
+		{
+			name: "Test with valid request",
+			request: KubernetesLabelsRequest{
+				Stream: "testStream",
+				Team:   "testTeam",
+				App:    "testApp",
+				Labels: []mlp.Label{
+					{Key: "customLabel1", Value: "value1"},
+					{Key: "customLabel2", Value: "value2"},
+				},
+			},
+			expected: map[string]string{
+				"orchestrator": "turing",
+				"stream":       "testStream",
+				"team":         "testTeam",
+				"app":          "testApp",
+				"environment":  "production",
+				"customLabel1": "value1",
+				"customLabel2": "value2",
+			},
+		},
+		{
+			name: "Test with reserved key",
+			request: KubernetesLabelsRequest{
+				Stream: "testStream",
+				Team:   "testTeam",
+				App:    "testApp",
+				Labels: []mlp.Label{
+					{Key: "orchestrator", Value: "value1"}, // Reserved key
+				},
+			},
+			expected: map[string]string{
+				"orchestrator": "turing", // Should not be overridden
+				"stream":       "testStream",
+				"team":         "testTeam",
+				"app":          "testApp",
+				"environment":  "production",
+			},
+		},
+		{
+			name: "Test with invalid label names",
+			request: KubernetesLabelsRequest{
+				Stream: "testStream",
+				Team:   "testTeam",
+				App:    "testApp",
+				Labels: []mlp.Label{
+					{Key: "invalid.Label", Value: "value1"}, // Invalid label key
+				},
+			},
+			expected: map[string]string{
+				"orchestrator": "turing",
+				"stream":       "testStream",
+				"team":         "testTeam",
+				"app":          "testApp",
+				"environment":  "production",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		// Always reset singleton objects.
+		defer InitKubernetesLabeller("", "dev")
+		t.Run(tt.name, func(t *testing.T) {
+			got := BuildLabels(tt.request)
+			if diff := cmp.Diff(got, tt.expected); diff != "" {
+				t.Errorf("BuildLabels() mismatch (-got +want):\n%s", diff)
 			}
 		})
 	}
