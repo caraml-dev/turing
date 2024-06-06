3565471f371a293acfd84b4d6055c8a759092ac5
ewezy <ziyi.ewe@gojek.com>
2024-06-07 01:07:07 +0800

    Move ensembler deletion logic to ensembler tojson method

--- ui/src/services/ensembler/Ensembler.js
+++ ui/src/services/ensembler/Ensembler.js
@@ -27,6 +27,9 @@ export class Ensembler {
   toJSON() {
     switch (this.type) {
       case "docker":
+        if (this.docker_config.resource_request?.cpu_limit === "") {
+          delete this.docker_config.resource_request.cpu_limit;
+        }
         return { type: this.type, docker_config: this.docker_config };
       case "standard":
         if (this.standard_config.experiment_mappings?.length === 0) {
@@ -35,11 +38,15 @@ export class Ensembler {
         if (this.standard_config.route_name_path === "") {
           delete this.standard_config.route_name_path;
         }
+        delete this.standard_config.fallback_response_route_id;
         return { type: this.type, standard_config: this.standard_config };
       case "pyfunc":
+        if (this.pyfunc_config.resource_request?.cpu_limit === "") {
+          delete this.pyfunc_config.resource_request.cpu_limit;
+        }
         return { type: this.type, pyfunc_config: this.pyfunc_config };
       default:
-        return { ...this, nop_config: this.nop_config };
+        return undefined;
     }
   }
 }
