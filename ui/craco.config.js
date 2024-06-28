91959dbfd6badfb05a87ad0112f704fb3e8de574
Arief Rahmansyah <arief.rahmansyah@gojek.com>
2024-06-28 09:55:44 +0700

    Upgrade UI deps

--- ui/craco.config.js
+++ ui/craco.config.js
@@ -3,56 +3,56 @@ const { ModuleFederationPlugin } = require("webpack").container;
 const { "react-lazylog": undefined, ...sharedDeps } = require("./package.json").dependencies;
 
 module.exports = ({ env }) => ({
-    plugins: [
-        {
-            plugin: {
-                overrideWebpackConfig: ({ webpackConfig }) => {
-                    // Suppress source-map related warnings (currently, react-use-dimensions is problematic).
-                    // This is setting was applied by CRA4, but CRA5 doesn't.
-                    webpackConfig.ignoreWarnings = [
-                        ...(webpackConfig.ignoreWarnings || []),
-                        /Failed to parse source map/,
-                    ];
-                    return webpackConfig;
-                },
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          // Suppress source-map related warnings (currently, react-use-dimensions is problematic).
+          // This is setting was applied by CRA4, but CRA5 doesn't.
+          webpackConfig.ignoreWarnings = [
+            ...(webpackConfig.ignoreWarnings || []),
+            /Failed to parse source map/,
+          ];
+          return webpackConfig;
+        },
+      },
+    }
+  ],
+  webpack: {
+    plugins: {
+      add: [
+        new ModuleFederationPlugin({
+          name: "turing",
+          filename: "remoteEntry.js",
+          exposes: {
+            ".": "./src/AppRoutes"
+          },
+          shared: {
+            ...sharedDeps,
+            "@emotion/react": {
+              singleton: true,
+              requiredVersion: sharedDeps["@emotion/react"]
             },
-        }
-    ],
-    webpack: {
-        plugins: {
-            add: [
-                new ModuleFederationPlugin({
-                    name: "turing",
-                    filename: "remoteEntry.js",
-                    exposes: {
-                        ".": "./src/AppRoutes"
-                    },
-                    shared: {
-                        ...sharedDeps,
-                        "@emotion/react": {
-                            singleton: true,
-                            requiredVersion: sharedDeps["@emotion/react"]
-                        },
-                        react: {
-                            shareScope: "default",
-                            singleton: true,
-                            requiredVersion: sharedDeps.react,
-                        },
-                        "react-dom": {
-                            singleton: true,
-                            requiredVersion: sharedDeps["react-dom"],
-                        },
-                        "react-router-dom": {
-                            singleton: true,
-                            requiredVersion: sharedDeps["react-router-dom"]
-                        },
-                        "@caraml-dev/ui-lib": {
-                            singleton: true,
-                            requiredVersion: sharedDeps["@caraml-dev/ui-lib"],
-                        }
-                    },
-                }),
-            ],
-        }
-    },
+            react: {
+              shareScope: "default",
+              singleton: true,
+              requiredVersion: sharedDeps.react,
+            },
+            "react-dom": {
+              singleton: true,
+              requiredVersion: sharedDeps["react-dom"],
+            },
+            "react-router-dom": {
+              singleton: true,
+              requiredVersion: sharedDeps["react-router-dom"]
+            },
+            "@caraml-dev/ui-lib": {
+              singleton: true,
+              requiredVersion: sharedDeps["@caraml-dev/ui-lib"],
+            }
+          },
+        }),
+      ],
+    }
+  },
 });
