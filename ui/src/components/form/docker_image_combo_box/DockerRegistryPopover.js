596d7ccb40d58c4d21af13844d4e3fa55aab82f2
Vinoth Kumar Sambath <s.vinothkumar@gojek.com>
2024-11-20 15:10:26 +0800

    Address review comments - remove extra space, fix indentation

--- ui/src/components/form/docker_image_combo_box/DockerRegistryPopover.js
+++ ui/src/components/form/docker_image_combo_box/DockerRegistryPopover.js
@@ -1,14 +1,14 @@
-import {  EuiSuperSelect } from "@elastic/eui";
+import { EuiSuperSelect } from "@elastic/eui";
 
 export const DockerRegistryPopover = ({ value, registryOptions, onChange }) => {
+
   return (
     <EuiSuperSelect
-          fullWidth
-          options={registryOptions}
-          valueOfSelected={value}
-          itemLayoutAlign="top"
-          onChange={onChange}
-          isInvalid={false}
-        />
+      fullWidth
+      options={registryOptions}
+      valueOfSelected={value}
+      itemLayoutAlign="top"
+      onChange={onChange}
+    />
   );
 };
