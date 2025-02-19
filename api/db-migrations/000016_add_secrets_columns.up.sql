d50bd56c6aebf748d35f36e8d85df8db349e866b
eweziyi <ziyi.ewe@gojek.com>
2025-02-19 16:23:08 +0800

    Update db migration scripts

--- api/db-migrations/000016_add_secrets_columns.up.sql
+++ api/db-migrations/000016_add_secrets_columns.up.sql
@@ -1,2 +1,7 @@
 -- Create secrets column for enrichers
 ALTER TABLE enrichers ADD COLUMN secrets jsonb NOT NULL DEFAULT '[]'::jsonb;
+
+-- Create secrets field in docker_config and pyfunc_config columns for ensemblers
+UPDATE ensembler_configs SET docker_config = jsonb_set(docker_config, '{secrets}', '[]'::jsonb) WHERE docker_config IS NOT NULL AND docker_config->'secrets' IS NULL;
+
+UPDATE ensembler_configs SET pyfunc_config = jsonb_set(pyfunc_config, '{secrets}', '[]'::jsonb) WHERE pyfunc_config IS NOT NULL AND pyfunc_config->'secrets' IS NULL;
