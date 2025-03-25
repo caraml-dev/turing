e11506b47c4e7b673fa165c68e0dbdbc8ccbd34d
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-03-25 13:39:58 +0700

    chore: add version when publishing pyfunc-ensembler-service package

--- engines/pyfunc-ensembler-service/setup.py
+++ engines/pyfunc-ensembler-service/setup.py
@@ -1,7 +1,12 @@
 import setuptools
 import pathlib
 import pkg_resources
+import imp
+import os
 
+version = imp.load_source(
+    "pyfuncserver.version", os.path.join("version.py")
+).VERSION
 
 with pathlib.Path("requirements.txt").open() as requirements_txt:
     requirements = [
@@ -17,8 +22,10 @@ with pathlib.Path("requirements.dev.txt").open() as dev_requirements_test:
 
 setuptools.setup(
     name="pyfunc-ensembler-service",
+    version=version,
     packages=setuptools.find_packages(),
     install_requires=requirements,
     dev_requirements=dev_requirements,
     python_requires=">=3.8,<3.11",
+    setup_requires=["setuptools_scm"],
 )
