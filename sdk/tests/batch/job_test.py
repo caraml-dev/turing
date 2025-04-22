e410c699fcaec75182f2f208a3b9c294745335c2
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-22 17:09:34 +0700

    feat: adjust test_terminate_job

--- sdk/tests/batch/job_test.py
+++ sdk/tests/batch/job_test.py
@@ -206,3 +206,70 @@ def test_fetch_job(
         job.refresh()
 
         assert job == updated
+        
+@pytest.mark.parametrize(
+    "job, api_response_delete, api_response_get, expected",
+    [
+        pytest.param(
+            turing.batch.EnsemblingJob(
+                id=1,
+                name="ensembling-job",
+                ensembler_id=1,
+                status=turing.batch.EnsemblingJobStatus.RUNNING,
+                project_id=1,
+                error="",
+                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
+                updated_at=utc_date("2021-07-06T13:28:56.252642Z"),
+            ),
+            '{"id": 1}',
+            get_job_0000,
+            turing.batch.EnsemblingJob(
+                id=1,
+                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
+                ensembler_id=2,
+                status=turing.batch.EnsemblingJobStatus.FAILED_BUILDING,
+                project_id=1,
+                error="timeout has occurred",
+                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
+                updated_at=utc_date("2021-07-07T00:00:00.252642Z"),
+            ),
+        )
+    ],
+)
+def test_terminate_job(
+    turing_api,
+    project,
+    job,
+    api_response_delete,
+    api_response_get,
+    expected,
+    use_google_oauth,
+    active_project_magic_mock
+):
+    with patch("urllib3.PoolManager.request") as mock_request:
+        turing.set_url(turing_api, use_google_oauth)
+
+        mock_request.return_value = active_project_magic_mock
+        turing.set_project(project.name)
+
+        mock_response_1 = MagicMock()
+        mock_response_1.method = "DELETE"
+        mock_response_1.status = 201
+        mock_response_1.path = f"/v1/projects/{project.id}/jobs/{job.id}"
+        mock_response_1.data = api_response_delete.encode('utf-8')
+        mock_response_1.getheader.return_value = 'application/json'
+        
+        mock_response_2 = MagicMock()
+        mock_response_2.method = "get"
+        mock_response_2.status = 200
+        mock_response_2.path = f"/v1/projects/{project.id}/jobs/{job.id}"
+        mock_response_2.data = api_response_get.encode('utf-8')
+        mock_response_2.getheader.return_value = 'application/json'
+        
+        mock_request.side_effect = [mock_response_1, mock_response_2]
+
+        assert job != expected
+
+        job.terminate()
+
+        assert job == expected
