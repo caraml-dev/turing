440b249758f9645c899e07fa6de2bef30ec45a8a
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-22 17:07:25 +0700

    feat: adjust test_fetch_job

--- sdk/tests/batch/job_test.py
+++ sdk/tests/batch/job_test.py
@@ -135,3 +135,74 @@ def test_submit_job(
             config=ensembling_job_config,
         )
         assert actual == expected
+
+@pytest.mark.parametrize(
+    "api_response_get, expected, api_response_refresh, updated",
+    [
+        pytest.param(
+            submit_job_0000,
+            turing.batch.EnsemblingJob(
+                id=1,
+                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
+                ensembler_id=2,
+                status=turing.batch.EnsemblingJobStatus.PENDING,
+                project_id=1,
+                error="",
+                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
+                updated_at=utc_date("2021-07-06T13:28:56.252642Z"),
+            ),
+            get_job_0000,
+            turing.batch.EnsemblingJob(
+                id=1,
+                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
+                ensembler_id=2,
+                status=turing.batch.EnsemblingJobStatus.FAILED_BUILDING,
+                project_id=1,
+                error="timeout has occurred",
+                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
+                updated_at=utc_date("2021-07-07T00:00:00.252642Z"),
+            ),
+        )
+    ],
+)
+def test_fetch_job(
+    turing_api,
+    project,
+    api_response_get,
+    expected,
+    api_response_refresh,
+    updated,
+    use_google_oauth,
+    active_project_magic_mock
+):
+    with patch("urllib3.PoolManager.request") as mock_request:
+        turing.set_url(turing_api, use_google_oauth)
+
+        mock_request.return_value = active_project_magic_mock
+        turing.set_project(project.name)
+
+        mock_response = MagicMock()
+        mock_response.method = "GET"
+        mock_response.status = 200
+        mock_response.path = f"/v1/projects/{project.id}/jobs/{expected.id}"
+        mock_response.data = api_response_get.encode('utf-8')
+        mock_response.getheader.return_value = 'application/json'
+        
+        mock_request.return_value = mock_response
+
+        job = turing.batch.EnsemblingJob.get_by_id(expected.id)
+
+        assert job == expected
+        
+        mock_response = MagicMock()
+        mock_response.method = "GET"
+        mock_response.status = 200
+        mock_response.path = f"/v1/projects/{project.id}/jobs/{expected.id}"
+        mock_response.data = api_response_refresh.encode('utf-8')
+        mock_response.getheader.return_value = 'application/json'
+        
+        mock_request.return_value = mock_response
+
+        job.refresh()
+
+        assert job == updated
