cbf6f6e5ea2f353fc1562434777ccb70b118b7a8
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-22 16:45:55 +0700

    feat: adjust test_list_jobs

--- sdk/tests/batch/job_test.py
+++ sdk/tests/batch/job_test.py
@@ -1,12 +1,14 @@
+import json
 import os
+from unittest.mock import MagicMock, patch
+
 import pytest
 from tests import utc_date
+import tests
 import turing
 import turing.batch
 import turing.batch.config
-from urllib3_mock import Responses
 
-responses = Responses("requests.packages.urllib3")
 data_dir = os.path.join(os.path.dirname(__file__), "../testdata/api_responses")
 
 with open(os.path.join(data_dir, "list_jobs_0000.json")) as f:
@@ -18,13 +20,6 @@ with open(os.path.join(data_dir, "submit_job_0000.json")) as f:
 with open(os.path.join(data_dir, "get_job_0000.json")) as f:
     get_job_0000 = f.read()
 
-
-@pytest.fixture(scope="module", name="responses")
-def _responses():
-    return responses
-
-
-@responses.activate
 @pytest.mark.parametrize(
     "api_response, expected",
     [
@@ -62,208 +57,40 @@ def _responses():
     ],
 )
 def test_list_jobs(
-    turing_api, active_project, api_response, expected, use_google_oauth
+    turing_api, project, api_response, expected, use_google_oauth
 ):
-    turing.set_url(turing_api, use_google_oauth)
-    turing.set_project(active_project.name)
-
-    responses.add(
-        method="GET",
-        url=f"/v1/projects/{active_project.id}/jobs?"
+    with patch("urllib3.PoolManager.request") as mock_request:
+        turing.set_url(turing_api, use_google_oauth)
+        
+        mock_response = MagicMock()
+        mock_response.method = "GET"
+        mock_response.status = 200
+        mock_response.path = f"/v1/projects?name={project.name}"
+        mock_response.data = json.dumps([project], default=tests.json_serializer).encode('utf-8')
+        mock_response.getheader.return_value = 'application/json'
+        
+        mock_request.return_value = mock_response
+        turing.set_project(project.name)
+        
+        mock_response = MagicMock()
+        mock_response.method = "GET"
+        mock_response.status = 200
+        mock_response.path = f"/v1/projects/{project.id}/jobs?"
         f"status={turing.batch.EnsemblingJobStatus.PENDING.value}&"
-        f"status={turing.batch.EnsemblingJobStatus.RUNNING.value}",
-        body=api_response,
-        match_querystring=True,
-        status=200,
-        content_type="application/json",
-    )
+        f"status={turing.batch.EnsemblingJobStatus.RUNNING.value}"
+        mock_response.data = api_response.encode('utf-8')
+        mock_response.getheader.return_value = 'application/json'
+        
+        mock_request.return_value = mock_response
 
-    actual = turing.batch.EnsemblingJob.list(
-        status=[
-            turing.batch.EnsemblingJobStatus.PENDING,
-            turing.batch.EnsemblingJobStatus.RUNNING,
-        ]
-    )
-
-    assert len(actual) == len(expected)
-
-    for actual, expected in zip(actual, expected):
-        assert actual == expected
-
-
-@responses.activate
-@pytest.mark.parametrize(
-    "api_response, expected",
-    [
-        pytest.param(
-            submit_job_0000,
-            turing.batch.EnsemblingJob(
-                id=1,
-                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
-                ensembler_id=2,
-                status=turing.batch.EnsemblingJobStatus.PENDING,
-                project_id=1,
-                error="",
-                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
-                updated_at=utc_date("2021-07-06T13:28:56.252642Z"),
-            ),
+        actual = turing.batch.EnsemblingJob.list(
+            status=[
+                turing.batch.EnsemblingJobStatus.PENDING,
+                turing.batch.EnsemblingJobStatus.RUNNING,
+            ]
         )
-    ],
-)
-def test_submit_job(
-    turing_api,
-    active_project,
-    ensembling_job_config,
-    api_response,
-    expected,
-    use_google_oauth,
-):
-    turing.set_url(turing_api, use_google_oauth)
-    turing.set_project(active_project.name)
 
-    responses.add(
-        method="POST",
-        url=f"/v1/projects/{active_project.id}/jobs",
-        body=api_response,
-        status=201,
-        content_type="application/json",
-    )
+        assert len(actual) == len(expected)
 
-    actual = turing.batch.job.EnsemblingJob.submit(
-        ensembler_id=2,
-        config=ensembling_job_config,
-    )
-    assert actual == expected
-
-
-@responses.activate
-@pytest.mark.parametrize(
-    "api_response_get, expected, api_response_refresh, updated",
-    [
-        pytest.param(
-            submit_job_0000,
-            turing.batch.EnsemblingJob(
-                id=1,
-                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
-                ensembler_id=2,
-                status=turing.batch.EnsemblingJobStatus.PENDING,
-                project_id=1,
-                error="",
-                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
-                updated_at=utc_date("2021-07-06T13:28:56.252642Z"),
-            ),
-            get_job_0000,
-            turing.batch.EnsemblingJob(
-                id=1,
-                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
-                ensembler_id=2,
-                status=turing.batch.EnsemblingJobStatus.FAILED_BUILDING,
-                project_id=1,
-                error="timeout has occurred",
-                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
-                updated_at=utc_date("2021-07-07T00:00:00.252642Z"),
-            ),
-        )
-    ],
-)
-def test_fetch_job(
-    turing_api,
-    active_project,
-    api_response_get,
-    expected,
-    api_response_refresh,
-    updated,
-    use_google_oauth,
-):
-    turing.set_url(turing_api, use_google_oauth)
-    turing.set_project(active_project.name)
-
-    responses.add(
-        method="GET",
-        url=f"/v1/projects/{active_project.id}/jobs/{expected.id}",
-        body=api_response_get,
-        status=200,
-        content_type="application/json",
-    )
-
-    job = turing.batch.EnsemblingJob.get_by_id(expected.id)
-
-    assert job == expected
-
-    responses.reset()
-    responses.add(
-        method="GET",
-        url=f"/v1/projects/{active_project.id}/jobs/{expected.id}",
-        body=api_response_refresh,
-        status=200,
-        content_type="application/json",
-    )
-
-    job.refresh()
-
-    assert job == updated
-
-
-@responses.activate
-@pytest.mark.parametrize(
-    "job, api_response_delete, api_response_get, expected",
-    [
-        pytest.param(
-            turing.batch.EnsemblingJob(
-                id=1,
-                name="ensembling-job",
-                ensembler_id=1,
-                status=turing.batch.EnsemblingJobStatus.RUNNING,
-                project_id=1,
-                error="",
-                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
-                updated_at=utc_date("2021-07-06T13:28:56.252642Z"),
-            ),
-            '{"id": 1}',
-            get_job_0000,
-            turing.batch.EnsemblingJob(
-                id=1,
-                name="pyfunc-ensembler: 2021-07-06T00:00:00+03:00",
-                ensembler_id=2,
-                status=turing.batch.EnsemblingJobStatus.FAILED_BUILDING,
-                project_id=1,
-                error="timeout has occurred",
-                created_at=utc_date("2021-07-06T12:28:32.850365Z"),
-                updated_at=utc_date("2021-07-07T00:00:00.252642Z"),
-            ),
-        )
-    ],
-)
-def test_terminate_job(
-    turing_api,
-    active_project,
-    job,
-    api_response_delete,
-    api_response_get,
-    expected,
-    use_google_oauth,
-):
-    turing.set_url(turing_api, use_google_oauth)
-    turing.set_project(active_project.name)
-
-    responses.add(
-        method="DELETE",
-        url=f"/v1/projects/{active_project.id}/jobs/{job.id}",
-        body=api_response_delete,
-        status=201,
-        content_type="application/json",
-    )
-
-    responses.add(
-        method="GET",
-        url=f"/v1/projects/{active_project.id}/jobs/{job.id}",
-        body=api_response_get,
-        status=200,
-        content_type="application/json",
-    )
-
-    assert job != expected
-
-    job.terminate()
-
-    assert job == expected
+        for actual, expected in zip(actual, expected):
+            assert actual == expected
