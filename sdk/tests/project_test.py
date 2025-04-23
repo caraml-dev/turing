bfbeaa6e8d41b66f23a8f3e54c7971ae5c465fa4
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-23 17:01:00 +0700

    feat: adjust test_list_projects

--- sdk/tests/project_test.py
+++ sdk/tests/project_test.py
@@ -1,35 +1,36 @@
 import json
+from unittest.mock import MagicMock, patch
 import tests
 import turing
 import pytest
-from urllib3_mock import Responses
 
-responses = Responses("requests.packages.urllib3")
-
-
-@responses.activate
 @pytest.mark.parametrize("num_projects", [10])
 def test_list_projects(turing_api, projects, use_google_oauth):
-    responses.add(
-        method="GET",
-        url=f"/v1/projects",
-        body=json.dumps(projects, default=tests.json_serializer),
-        status=200,
-        content_type="application/json",
-    )
+    with patch("urllib3.PoolManager.request") as mock_request:
+        mock_response = MagicMock()
+        mock_response.method = "GET"
+        mock_response.status = 200
+        mock_response.path = f"/v1/projects"
+        mock_response.data = json.dumps(projects, default=tests.json_serializer).encode('utf-8')
+        mock_response.getheader.return_value = 'application/json'
+   
+        mock_request.return_value = mock_response
 
-    turing.set_url(turing_api, use_google_oauth)
-    actual = turing.Project.list()
+        turing.set_url(turing_api, use_google_oauth)
+        actual = turing.Project.list()
 
-    assert len(actual) == len(projects)
-    assert all([isinstance(p, turing.Project) for p in actual])
+        assert len(actual) == len(projects)
+        assert all([isinstance(p, turing.Project) for p in actual])
 
-    for actual, expected in zip(actual, projects):
-        assert actual.id == expected.id
-        assert actual.name == expected.name
-        assert actual.mlflow_tracking_url == expected.mlflow_tracking_url
-        assert actual.created_at == expected.created_at
-        assert actual.updated_at == expected.updated_at
+        for actual, expected in zip(actual, projects):
+            assert actual.id == expected.id
+            assert actual.name == expected.name
+            assert actual.mlflow_tracking_url == expected.mlflow_tracking_url
+            assert actual.created_at == expected.created_at
+            assert actual.updated_at == expected.updated_at
 
-    turing.Project.list(name="project_1")
-    assert responses.calls[1].request.url == "/v1/projects?name=project_1"
+        turing.Project.list(name="project_1")
+        
+        args, kwargs = mock_request.call_args_list[1]
+        assert args[1] == "http://turing.local.svc:8080/v1/projects"
+        assert kwargs.get("fields") == [('name', 'project_1')]
