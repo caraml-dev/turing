48e50fdb84115e718bb5fdae34044f96840e49c1
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-23 16:19:57 +0700

    feat: refactor magic mock for mlflow and gcs to fixtures

--- sdk/tests/conftest.py
+++ sdk/tests/conftest.py
@@ -797,3 +797,107 @@ def active_project_magic_mock(project) -> MagicMock:
     mock_response.getheader.return_value = 'application/json'
     
     return mock_response
+
+@pytest.fixture
+def ensembler_mlflow_magic_mock_sequence(experiment_name, experiment_id, run_id, artifact_uri) -> list[MagicMock]:
+    mlflow_resp_1 = MagicMock()
+    mlflow_resp_1.method = "GET"
+    mlflow_resp_1.status_code = 200
+    mlflow_resp_1.path = f"/api/2.0/mlflow/experiments/get-by-name?experiment_name={quote_plus(experiment_name)}"
+    mlflow_resp_1.text = json.dumps(
+            {
+                "experiment": {
+                    "id": experiment_id,
+                    "name": experiment_name,
+                    "lifecycle_stage": "active",
+                }
+            }
+        )
+    mlflow_resp_1.headers = {'Content-Type': 'application/json'}
+    
+    mlflow_resp_2 = MagicMock()
+    mlflow_resp_2.method = "POST"
+    mlflow_resp_2.status_code = 200
+    mlflow_resp_2.path = "/api/2.0/mlflow/runs/create"
+    mlflow_resp_2.text = json.dumps(
+            {
+                "run": {
+                    "info": {
+                        "run_id": run_id,
+                        "experiment_id": experiment_id,
+                        "status": "RUNNING",
+                        "artifact_uri": artifact_uri,
+                        "lifecycle_stage": "active",
+                    },
+                    "data": {},
+                }
+            }
+        )
+    mlflow_resp_2.headers = {'Content-Type': 'application/json'}
+    
+    mlflow_resp_3 = MagicMock()
+    mlflow_resp_3.method = "GET"
+    mlflow_resp_3.status_code = 200
+    mlflow_resp_3.path = f"/api/2.0/mlflow/runs/get?run_uuid={run_id}&run_id={run_id}"
+    mlflow_resp_3.text = json.dumps(
+            {
+                "run": {
+                    "info": {
+                        "run_id": run_id,
+                        "experiment_id": experiment_id,
+                        "status": "RUNNING",
+                        "artifact_uri": artifact_uri,
+                        "lifecycle_stage": "active",
+                    },
+                    "data": {},
+                }
+            }
+        )
+    mlflow_resp_3.headers = {'Content-Type': 'application/json'}
+    
+    mlflow_resp_4 = MagicMock()
+    mlflow_resp_4.method = "POST"
+    mlflow_resp_4.status_code = 200
+    mlflow_resp_4.path = "/api/2.0/mlflow/runs/log-model"
+    mlflow_resp_4.text = json.dumps({})
+    mlflow_resp_4.headers = {'Content-Type': 'application/json'}
+    
+    mlflow_resp_5 = MagicMock()
+    mlflow_resp_5.method = "POST"
+    mlflow_resp_5.status_code = 200
+    mlflow_resp_5.path = "/api/2.0/mlflow/runs/update"
+    mlflow_resp_5.text = json.dumps({})
+    mlflow_resp_5.headers = {'Content-Type': 'application/json'}
+    
+    mlflow_resp_6 = MagicMock()
+    mlflow_resp_6.method = "POST"
+    mlflow_resp_6.status_code = 200
+    mlflow_resp_6.path = "/api/2.0/mlflow/runs/update"
+    mlflow_resp_6.text = json.dumps({})
+    mlflow_resp_6.headers = {'Content-Type': 'application/json'}
+    
+    return [mlflow_resp_1, mlflow_resp_2, mlflow_resp_3, mlflow_resp_4, mlflow_resp_5, mlflow_resp_6]
+
+@pytest.fixture
+def ensembler_gcs_magic_mock_sequence(bucket_name) -> list[MagicMock]:
+    gcs_resp_1 = MagicMock()
+    gcs_resp_1.method = "POST"
+    gcs_resp_1.status_code = 200
+    gcs_resp_1.path = "/token"
+    gcs_resp_1.text = json.dumps(
+            {
+                "access_token": "ya29.ImCpB6BS2mdOMseaUjhVlHqNfAOz168XjuDrK7Sd33glPd7XvtMLIngi1-V52ReytFSUluE-iBV88OlDkjtraggB_qc-LN2JlGtQ3sHZq_MuTxrU0-oK_kpq-1wsvniFFGQ",
+                "expires_in": 3600,
+                "scope": "openid https://www.googleapis.com/auth/cloud-platform https://www.googleapis.com/auth/userinfo.email",
+                "token_type": "Bearer",
+                "id_token": "eyJhbGciOiJSUzI1NiIsImtpZCI6IjhhNjNmZTcxZTUzMDY3NTI0Y2JiYzZhM2E1ODQ2M2IzODY0YzA3ODciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI3NjQwODYwNTE4NTAtNnFyNHA2Z3BpNmhuNTA2cHQ4ZWp1cTgzZGkzNDFodXIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI3NjQwODYwNTE4NTAtNnFyNHA2Z3BpNmhuNTA2cHQ4ZWp1cTgzZGkzNDFodXIuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDM5ODg0MzM2OTY3NzI1NDkzNjAiLCJoZCI6ImdvLWplay5jb20iLCJlbWFpbCI6InByYWRpdGh5YS5wdXJhQGdvLWplay5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiYXRfaGFzaCI6ImdrbXIxY0dPTzNsT0dZUDhtYjNJRnciLCJpYXQiOjE1NzE5Njg3NDUsImV4cCI6MTU3MTk3MjM0NX0.FIY5xvySNVxt1cbw-QXdDfiwollxcqupz1YDJuP14obKRyDwFG9ZcC_j-mTDZF5_dzpYeNMMK-LPTq9QIaM-blSKm2Eh9LeMvQGUk_S-9y_r2jKCmBlrEeHM8DXk3xyKf65LEoBA8cwMPdgb2s8AMIxxN9JJ09fjou20yLDI84Q4BFMriMIBBYLFgBW0wcg2PQ1hy5hrV1PdZj-ZNKNWmouh0lOjLLYmVFZPCzD9ENWo1N52ZLaLODdI2gDcpbyTUbeAh81sacdtJd0pLf-FuBLdfuktvP4MVvdmIhXv98Zb0dFBzRtmiqlQusSjoG5VEaBc6o2gkM5rHR0ozby0Fg",
+            }
+        )
+    
+    gcs_resp_2 = MagicMock()
+    gcs_resp_2.method = "POST"
+    gcs_resp_2.status_code = 200
+    gcs_resp_2.path = f"/upload/storage/v1/b/{bucket_name}/o?uploadType=multipart"
+    gcs_resp_2.text = json.dumps({})
+    
+    return [gcs_resp_1, gcs_resp_2]
