d30560cb1bd7fe31f8e14974aa57f0534b676c98
Muhammad Naufal Andika Natsir Putra <muhammad.putra@gojek.com>
2025-04-25 15:44:29 +0700

    feat: adjust how to get version from version.py

--- engines/pyfunc-ensembler-job/setup.py
+++ engines/pyfunc-ensembler-job/setup.py
@@ -1,18 +1,12 @@
 import setuptools
 import pathlib
 import pkg_resources
-import importlib.util
-import os
 
 # get version from version.py
-spec = importlib.util.spec_from_file_location(
-    "pyfuncserver.version", os.path.join("version.py")
-)
-
-v_module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(v_module)
-
-version = v_module.VERSION
+with pathlib.Path("version.py").open() as version_py:
+    _locals = locals()
+    exec(version_py.read(), globals(), _locals)
+    version = _locals["VERSION"]
 
 with pathlib.Path("requirements.txt").open() as requirements_txt:
     requirements = [
